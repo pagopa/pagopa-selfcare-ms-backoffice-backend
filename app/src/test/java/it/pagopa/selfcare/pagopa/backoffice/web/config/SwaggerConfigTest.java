c255efab4e44e2f085e0536aabc3f993112c3716
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-09-01 17:51:07 +0200

    changed names in swaggerconfig

--- app/src/test/java/it/pagopa/selfcare/pagopa/backoffice/web/config/SwaggerConfigTest.java
+++ app/src/test/java/it/pagopa/selfcare/pagopa/backoffice/web/config/SwaggerConfigTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -29,10 +30,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 })
 @EnableOpenApi
 @EnableWebMvc
-@ComponentScan(basePackages = "it.pagopa.selfcare.pagopa.web.controller")
+@ComponentScan(basePackages = "it.pagopa.selfcare.pagopa.backoffice.web.controller")
 @TestPropertySource(locations = "classpath:config/application.yml")
 class SwaggerConfigTest {
-    
+
     @MockBean
     private ApiManagementService managementService;
 
@@ -40,19 +41,20 @@ class SwaggerConfigTest {
     WebApplicationContext context;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    ObjectMapper objectMapper;
 
     @Test
     void swaggerSpringPlugin() throws Exception {
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
         mockMvc.perform(MockMvcRequestBuilders.get("/v3/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andDo((result) -> {
                     assertNotNull(result);
                     assertNotNull(result.getResponse());
                     final String content = result.getResponse().getContentAsString();
                     assertFalse(content.isBlank());
                     assertFalse(content.contains("${"), "Generated swagger contains placeholders");
-                    Object swagger = objectMapper.readValue(content, Object.class);
+                    Object swagger = objectMapper.readValue(result.getResponse().getContentAsString(), Object.class);
                     String formatted = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(swagger);
                     Path basePath = Paths.get("src/main/resources/swagger/");
                     Files.createDirectories(basePath);
