149a31ccbeba1335b700e2811a241b1f0ad86844
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-09-02 16:55:13 +0200

    add rest module and external api rest client

---
+++ connector/rest/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/client/ExternalApiRestClient.java
@@ -0,0 +1,17 @@
+package it.pagopa.selfcare.pagopa.backoffice.connector.rest.client;
+
+import it.pagopa.selfcare.pagopa.backoffice.connector.model.institution.Institution;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@FeignClient(name = "${rest-client.external-api.serviceCode}", url = "${rest-client.external-api.base-url}")
+public interface ExternalApiRestClient {
+
+    @GetMapping(value = "${rest-client.external-api.getInstitution.path}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    Institution getInstitution(@PathVariable(value = "id") String id);
+    
+}
