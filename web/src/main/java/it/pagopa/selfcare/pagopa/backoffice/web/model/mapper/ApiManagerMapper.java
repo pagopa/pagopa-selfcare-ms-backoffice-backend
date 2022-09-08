57ca88f34a71ea363de7e1f972eabc53f9706da8
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-09-08 10:44:53 +0200

    modified ApiManagerMapper to map to apiKey resource

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/mapper/ApiManagerMapper.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/mapper/ApiManagerMapper.java
@@ -1,30 +1,18 @@
 package it.pagopa.selfcare.pagopa.backoffice.web.model.mapper;
 
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.UserSubscription;
+import it.pagopa.selfcare.pagopa.backoffice.connector.model.InstitutionApiKeys;
 import it.pagopa.selfcare.pagopa.backoffice.web.model.subscriptions.ApiKeysResource;
 
 public class ApiManagerMapper {
 
-    public static ApiKeysResource toSubscriptionResource(UserSubscription model) {
+    public static ApiKeysResource toApiKeysResource(InstitutionApiKeys model) {
         ApiKeysResource resource = null;
         if (model != null) {
             resource = new ApiKeysResource();
-//            resource.setId(model.getId());
-//            resource.setName(model.getName());
             resource.setPrimaryKey(model.getPrimaryKey());
             resource.setSecondaryKey(model.getSecondaryKey());
         }
         return resource;
     }
 
-//    public static CreateInstitutionSubscription fromDto(CreateInstitutionSubscriptionDto dto) {
-//        CreateInstitutionSubscription subscription = null;
-//        if (dto != null) {
-//            subscription = new CreateInstitutionSubscription();
-//            subscription.setDescription(dto.getDescription());
-//            subscription.setEmail(dto.getEmail());
-//            subscription.setExternalId(dto.getExternalId());
-//        }
-//        return subscription;
-//    }
 }
