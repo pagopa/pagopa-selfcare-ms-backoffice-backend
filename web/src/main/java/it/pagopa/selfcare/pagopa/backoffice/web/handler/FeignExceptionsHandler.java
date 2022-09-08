ce7c3c411cd9bce554ab4faba7a8650a5e51fc65
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-09-08 10:45:44 +0200

    add Exception handlers

---
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/handler/FeignExceptionsHandler.java
@@ -0,0 +1,44 @@
+package it.pagopa.selfcare.pagopa.backoffice.web.handler;
+
+import feign.FeignException;
+import it.pagopa.selfcare.pagopa.backoffice.web.model.Problem;
+import it.pagopa.selfcare.pagopa.backoffice.web.model.mapper.ProblemMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+import static it.pagopa.selfcare.pagopa.backoffice.web.handler.RestExceptionsHandler.UNHANDLED_EXCEPTION;
+
+
+@Slf4j
+@RestControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
+@ConditionalOnClass(FeignException.class)
+public class FeignExceptionsHandler {
+
+    public FeignExceptionsHandler() {
+        log.trace("Initializing {}", FeignExceptionsHandler.class.getSimpleName());
+    }
+
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<Problem> handleFeignException(FeignException e) {
+        HttpStatus httpStatus = Optional.ofNullable(HttpStatus.resolve(e.status()))
+                .filter(status -> !status.is2xxSuccessful())
+                .orElse(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (httpStatus.is4xxClientError()) {
+            log.warn(e.toString());
+        } else {
+            log.error(UNHANDLED_EXCEPTION, e);
+        }
+        return ProblemMapper.toResponseEntity(new Problem(httpStatus, "An error occurred during a downstream service request"));
+    }
+
+}
