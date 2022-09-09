7dda98346f69498afddab7d87623280345f7dadd
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-09-09 15:54:06 +0200

    add web module tests

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/validator/PagopaBackofficeControllerResponseValidator.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/validator/PagopaBackofficeControllerResponseValidator.java
@@ -1,19 +1,60 @@
 package it.pagopa.selfcare.pagopa.backoffice.web.validator;
 
+import it.pagopa.selfcare.pagopa.backoffice.web.exception.ResponseValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-
+import java.util.*;
+@Slf4j
 @Aspect
 @Component
 public class PagopaBackofficeControllerResponseValidator {
 
-    @Autowired
+
+    private final Validator validator;
+
+
     public PagopaBackofficeControllerResponseValidator(Validator validator) {
+        log.trace("Initializing {}", PagopaBackofficeControllerResponseValidator.class.getSimpleName());
+        Assert.notNull(validator, "Validator is required");
+        this.validator = validator;
     }
+
+
+    @AfterReturning(pointcut = "controllersPointcut()", returning = "result")
+    public void validateResponse(JoinPoint joinPoint, Object result) {
+        log.trace("[validateResponse] start");
+        log.debug("[validateResponse] inputs: result = {}", result);
+        if (result != null) {
+            if (Collection.class.isAssignableFrom(result.getClass())) {
+                ((Collection<?>) result).forEach(this::validate);
+            } else {
+                validate(result);
+            }
+        }
+        log.trace("[validateResponse] end");
+    }
+
+    private void validate(Object result) {
+        Set<ConstraintViolation<Object>> validationResults = validator.validate(result);
+        if (!validationResults.isEmpty()) {
+            Map<String, List<String>> errorMessage = new HashMap<>();
+            validationResults.forEach(error -> {
+                String fieldName = error.getPropertyPath().toString();
+                errorMessage.computeIfAbsent(fieldName, s -> new ArrayList<>())
+                        .add(error.getConstraintDescriptor().getAnnotation().annotationType().getSimpleName() + " constraint violation");
+            });
+            throw new ResponseValidationException(errorMessage.toString());
+        }
+    }
+
 
     @Pointcut("execution(* it.pagopa.selfcare.pagopa.backoffice.web.controller.*.*(..))")
     public void controllersPointcut() {
---
+++ web/src/test/java/it/pagopa/selfcare/pagopa/backoffice/web/model/subscriptions/ApiKeysResourceTest.java
@@ -0,0 +1,64 @@
+package it.pagopa.selfcare.pagopa.backoffice.web.model.subscriptions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotBlank;
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static it.pagopa.selfcare.pagopa.TestUtils.mockInstance;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApiKeysResourceTest {
+
+    private Validator validator;
+
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+    
+    @Test
+    void validateNullFields(){
+        //given
+        HashMap<String, Class<? extends Annotation>> toCheckMap = new HashMap<>();
+        toCheckMap.put("primaryKey", NotBlank.class);
+        toCheckMap.put("secondaryKey", NotBlank.class);
+        ApiKeysResource model = new ApiKeysResource();
+        model.setPrimaryKey(null);
+        model.setSecondaryKey(null);
+        //when
+        Set<ConstraintViolation<Object>> violations = validator.validate(model);
+        // then
+        assertFalse(violations.isEmpty());
+        List<ConstraintViolation<Object>> filteredViolations = violations.stream()
+                .filter(violation -> {
+                    Class<? extends Annotation> annotationToCheck = toCheckMap.get(violation.getPropertyPath().toString());
+                    return !violation.getConstraintDescriptor().getAnnotation().annotationType().equals(annotationToCheck);
+                })
+                .collect(Collectors.toList());
+        assertTrue(filteredViolations.isEmpty());
+    }
+
+    @Test
+    void validateNotNullFields(){
+        //given
+        ApiKeysResource resource = mockInstance(new ApiKeysResource());
+        // when
+        Set<ConstraintViolation<Object>> violations = validator.validate(resource);
+        // then
+        assertTrue(violations.isEmpty());
+    }
+
+}
