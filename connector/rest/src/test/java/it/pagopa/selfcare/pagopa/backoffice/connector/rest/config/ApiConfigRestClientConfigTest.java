e29276857047d78ec61e0699baaea8b5f44607f2
Biagio Murano <biagio.murano@emea.nttdata.com>
2023-01-26 14:43:49 +0100

    fixed smell warning for test class which does not provide any test method inside

--- connector/rest/src/test/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/config/ApiConfigRestClientConfigTest.java
+++ connector/rest/src/test/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/config/ApiConfigRestClientConfigTest.java
@@ -1,7 +1,9 @@
 package it.pagopa.selfcare.pagopa.backoffice.connector.rest.config;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.context.annotation.Import;
 
 @Import(ApiConfigRestClientConfig.class)
+@Disabled("this class has no test class inside it")
 public class ApiConfigRestClientConfigTest {
 }
