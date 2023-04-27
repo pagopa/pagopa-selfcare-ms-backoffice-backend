0706c4f5dd8906b8efa768f4a72f1af24752fe64
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2023-04-27 17:11:10 +0200

    Feature/paselc 275bis (#107)
    Co-authored-by: Biagio Murano <biagio.murano@emea.nttdata.com>
    Co-authored-by: Fabio Lisco <12395006+fmlisco@users.noreply.github.com>
    Co-authored-by: biaMurNtt <117842827+biaMurNtt@users.noreply.github.com>

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/mapper/StationMapper.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/mapper/StationMapper.java
@@ -3,10 +3,7 @@ package it.pagopa.selfcare.pagopa.backoffice.web.model.mapper;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.Station;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.StationDetails;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.Stations;
-import it.pagopa.selfcare.pagopa.backoffice.web.model.stations.StationDetailResource;
-import it.pagopa.selfcare.pagopa.backoffice.web.model.stations.StationDetailsDto;
-import it.pagopa.selfcare.pagopa.backoffice.web.model.stations.StationResource;
-import it.pagopa.selfcare.pagopa.backoffice.web.model.stations.StationsResource;
+import it.pagopa.selfcare.pagopa.backoffice.web.model.stations.*;
 
 //@Mapper
 public interface StationMapper {
@@ -18,5 +15,7 @@ public interface StationMapper {
     StationResource toResource(Station model);
 
     StationDetails fromDto(StationDetailsDto model);
+
+    StationDetails fromWrapperStationDetailsDto(WrapperStationDetailsDto model);
 
 }
