73b5346395d80d541190ac78a6927429d8730294
Biagio Murano <biagio.murano@emea.nttdata.com>
2023-06-01 16:41:23 +0200

    fix blank costraint

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/creditorInstituions/CreditorInstitutionAddressResource.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/creditorInstituions/CreditorInstitutionAddressResource.java
@@ -8,22 +8,17 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class CreditorInstitutionAddressResource {
     @ApiModelProperty(value = "${swagger.creditor-institutions.model.address.location}", required = true)
-    @NotBlank
     private String location;
 
     @ApiModelProperty(value = "${swagger.creditor-institutions.model.address.city}", required = true)
-    @NotBlank
     private String city;
 
     @ApiModelProperty(value = "${swagger.creditor-institutions.model.address.zipCode}", required = true)
-    @NotBlank
     private String zipCode;
 
     @ApiModelProperty(value = "${swagger.creditor-institutions.model.address.countryCode}", required = true)
-    @NotBlank
     private String countryCode;
 
     @ApiModelProperty(value = "${swagger.creditor-institutions.model.address.taxDomicile}", required = true)
-    @NotBlank
     private String taxDomicile;
 }
