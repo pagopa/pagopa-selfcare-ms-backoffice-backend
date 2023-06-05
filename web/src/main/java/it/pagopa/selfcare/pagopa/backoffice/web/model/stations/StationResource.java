88ab719144d67545a0d1a27a0d7034e9e095cab9
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2023-06-05 22:49:32 +0200

    Feature/fix05062023 (#133)

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/stations/StationResource.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/stations/StationResource.java
@@ -3,6 +3,7 @@ package it.pagopa.selfcare.pagopa.backoffice.web.model.stations;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.StationStatus;
+import it.pagopa.selfcare.pagopa.backoffice.connector.model.wrapper.WrapperStatus;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -29,7 +30,7 @@ public class StationResource {
     @ApiModelProperty(value = "${swagger.model.station.status}", required = true)
     @JsonProperty(required = true)
     @NotNull
-    private StationStatus stationStatus;
+    private WrapperStatus wrapperStatus;
     @ApiModelProperty("${swagger.model.station.associatedCreditorInstitutions}")
     @JsonProperty(required = true)
     @NotNull
