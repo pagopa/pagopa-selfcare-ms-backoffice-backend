7ba7e018bb0704ef0f646fa49dc1668e1c68f028
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2023-06-07 16:58:34 +0200

    Feature/paselc 346 (#138)

--- connector/rest/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/interceptor/AuthorizationApiConfigHeaderInterceptor.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/interceptor/AuthorizationApiConfigHeaderInterceptor.java
@@ -2,6 +2,7 @@ package it.pagopa.selfcare.pagopa.backoffice.connector.rest.interceptor;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import it.pagopa.selfcare.pagopa.backoffice.connector.exception.PermissionDeniedException;
 import it.pagopa.selfcare.pagopa.backoffice.connector.security.SelfCareUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +11,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.naming.NoPermissionException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -19,25 +19,31 @@ public class AuthorizationApiConfigHeaderInterceptor implements RequestIntercept
     @Value("${authorization.api-config.subscriptionKey}")
     private String apiConfigSubscriptionKey;
 
-    private static final List<String> PARAMS_NAME = List.of("","");
+    @Value("${authorization.api-config.flag-authorization}")
+    private boolean flagAuthorization;
+
+    private static final List<String> PARAMS_NAME = List.of(
+            "stationId", "ecCode", "stationcode", "code", "brokerId", "stationCode", "creditorInstitutionCode",
+            "brokerCode", "pspcode", "channelcode", "brokerpspcode", "channelId");
+
     @Override
     public void apply(RequestTemplate template) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         SelfCareUser user = (SelfCareUser) auth.getPrincipal();
-        check(PARAMS_NAME,template,user);
+        if (flagAuthorization) {
+            check(template, user);
+        }
         template.header("x-selfcare-uid", user.getId());
         template.removeHeader("Ocp-Apim-Subscription-Key")
                 .header("Ocp-Apim-Subscription-Key", apiConfigSubscriptionKey);
     }
 
-    public void check(String paramName,RequestTemplate template,SelfCareUser user) {
-        if((template.queries().containsKey(paramName)&& !(template.queries().get(paramName).toArray()[0]==user.getOrgVat())))
-            throw new RuntimeException(new NoPermissionException(""));
-
+    void check(String paramName, RequestTemplate template, SelfCareUser user) {
+        if ((template.queries().containsKey(paramName) && !(template.queries().get(paramName).contains(user.getOrgVat()))))
+            throw new PermissionDeniedException("This action is not permitted by current user");
     }
 
-    private void check(List<String> paramsName,RequestTemplate template,SelfCareUser user) {
-       paramsName.forEach(param->check(param,template,user));
-
+    void check(RequestTemplate template, SelfCareUser user) {
+        AuthorizationApiConfigHeaderInterceptor.PARAMS_NAME.forEach(param -> check(param, template, user));
     }
 }
