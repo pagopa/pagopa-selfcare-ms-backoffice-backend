078a30a78921462b11f278d2e74e957b062f4f81
GiacomoBrancazi <giacomo.brancazi@gmail.com>
2023-06-07 15:56:41 +0200

    add coverage

--- connector/rest/src/test/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/interceptor/AuthorizationApiConfigHeaderInterceptorTest.java
+++ connector/rest/src/test/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/interceptor/AuthorizationApiConfigHeaderInterceptorTest.java
@@ -2,17 +2,21 @@ package it.pagopa.selfcare.pagopa.backoffice.connector.rest.interceptor;
 
 import feign.RequestTemplate;
 import it.pagopa.selfcare.pagopa.backoffice.connector.security.SelfCareUser;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class AuthorizationApiConfigHeaderInterceptorTest {
  
@@ -28,6 +32,22 @@ public class AuthorizationApiConfigHeaderInterceptorTest {
         SecurityContext securityContext = mock(SecurityContext.class);
         when(securityContext.getAuthentication()).thenReturn(mock(Authentication.class));
         SecurityContextHolder.setContext(securityContext);
+    }
+
+    @Test
+    public void testApply() {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        RequestTemplate template = new RequestTemplate();
+        user = mock(SelfCareUser.class);
+
+        ReflectionTestUtils.setField(interceptor, "apiConfigSubscriptionKey", "sub-key");
+        ReflectionTestUtils.setField(interceptor, "flagAuthorization", true);
+
+        when(auth.getPrincipal()).thenReturn(user);
+        when(user.getId()).thenReturn("user-id");
+
+        interceptor.apply(template);
     }
 
     @Test
