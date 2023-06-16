deb01d8f8b94e1aacee34f697c6f7c5de3216587
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2023-06-16 14:56:38 +0200

    Feature/paselc 352 (#159)

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/channels/ChannelDetailsResource.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/channels/ChannelDetailsResource.java
@@ -25,7 +25,6 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("protocol")
     @ApiModelProperty(value = "${swagger.model.channel.details.protocol}")
-    @NotNull
     private Protocol protocol;
 
     @JsonProperty("ip")
@@ -34,7 +33,6 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("port")
     @ApiModelProperty(value = "${swagger.model.channel.details.port}")
-    @NotNull
     private Long port;
 
     @JsonProperty("service")
@@ -43,7 +41,6 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("broker_psp_code")
     @ApiModelProperty(value = "${swagger.model.channel.details.brokerPspCode}")
-    @NotBlank
     private String brokerPspCode;
 
     @JsonProperty("proxy_enabled")
@@ -82,22 +79,18 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("thread_number")
     @ApiModelProperty(value = "${swagger.model.channel.details.threadNumber}")
-    @NotNull
     private Long threadNumber;
 
     @JsonProperty("timeout_a")
     @ApiModelProperty(value = "${swagger.model.channel.details.timeoutA}")
-    @NotNull
     private Long timeoutA;
 
     @JsonProperty("timeout_b")
     @ApiModelProperty(value = "${swagger.model.channel.details.timeoutB}")
-    @NotNull
     private Long timeoutB;
 
     @JsonProperty("timeout_c")
     @ApiModelProperty(value = "${swagger.model.channel.details.timeoutC}")
-    @NotNull
     private Long timeoutC;
 
     @JsonProperty("nmp_service")
@@ -133,7 +126,6 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("payment_model")
     @ApiModelProperty(value = "${swagger.model.channel.details.paymentModel}")
-    @NotNull
     private PaymentModel paymentModel;
 
     @JsonProperty("serv_plugin")
@@ -142,27 +134,22 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("rt_push")
     @ApiModelProperty(value = "${swagger.model.channel.details.rtPush}")
-    @NotNull
     private Boolean rtPush;
 
     @JsonProperty("on_us")
     @ApiModelProperty(value = "${swagger.model.channel.details.onUs}")
-    @NotNull
     private Boolean onUs;
 
     @JsonProperty("card_chart")
     @ApiModelProperty(value = "${swagger.model.channel.details.cardChart}")
-    @NotNull
     private Boolean cardChart;
 
     @JsonProperty("recovery")
     @ApiModelProperty(value = "${swagger.model.channel.details.recovery}")
-    @NotNull
     private Boolean recovery;
 
     @JsonProperty("digital_stamp_brand")
     @ApiModelProperty(value = "${swagger.model.channel.details.digitalStampBrand}")
-    @NotNull
     private Boolean digitalStampBrand;
 
     @JsonProperty("flag_io")
@@ -171,7 +158,6 @@ public class ChannelDetailsResource extends ChannelResource {
 
     @JsonProperty("agid")
     @ApiModelProperty(value = "${swagger.model.channel.details.agid}")
-    @NotNull
     private Boolean agid;
 
     @JsonProperty("payment_types")
