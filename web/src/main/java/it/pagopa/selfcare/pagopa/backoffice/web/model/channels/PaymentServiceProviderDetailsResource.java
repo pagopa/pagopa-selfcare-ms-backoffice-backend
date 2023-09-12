a0c7fb466bf41440ba70e65eb2b9d8be984622d9
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2023-09-12 10:09:34 +0200

    remove required on fields PSP DetailResorce

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/channels/PaymentServiceProviderDetailsResource.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/model/channels/PaymentServiceProviderDetailsResource.java
@@ -8,31 +8,31 @@ import lombok.Data;
 public class PaymentServiceProviderDetailsResource extends  PaymentServiceProviderResource{
 
     @JsonProperty("abi")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.abi}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.abi}")
     private String abi;
 
     @JsonProperty("bic")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.bic}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.bic}")
     private String bic;
 
     @JsonProperty("my_bank_code")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.myBankCode}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.myBankCode}")
     private String myBankCode;
 
     @JsonProperty("stamp")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.stamp}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.stamp}")
     private Boolean stamp;
 
     @JsonProperty("agid_psp")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.agidPsp}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.agidPsp}")
     private Boolean agidPsp = false;
 
     @JsonProperty("tax_code")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.taxCode}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.taxCode}")
     private String taxCode;
 
     @JsonProperty("vat_number")
-    @ApiModelProperty(value = "${swagger.model.pspDetails.vatNumber}", required = true)
+    @ApiModelProperty(value = "${swagger.model.pspDetails.vatNumber}")
     private String vatNumber;
 }
 
