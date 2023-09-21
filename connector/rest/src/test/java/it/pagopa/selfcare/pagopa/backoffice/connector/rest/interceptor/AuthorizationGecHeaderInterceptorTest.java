dbaf4b27a01e909c7c0a49fc845da0cae8184c61
Biagio Murano <biagio.murano@emea.nttdata.com>
2023-09-21 11:19:44 +0200

    wip feign client gec

---
+++ connector/rest/src/test/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/interceptor/AuthorizationGecHeaderInterceptorTest.java
@@ -0,0 +1,49 @@
+package it.pagopa.selfcare.pagopa.backoffice.connector.rest.interceptor;
+
+import feign.RequestTemplate;
+import it.pagopa.selfcare.pagopa.backoffice.connector.security.SelfCareUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AuthorizationGecHeaderInterceptorTest {
+
+    private AuthorizationGecHeaderInterceptor interceptor;
+    private RequestTemplate template;
+    private SelfCareUser user;
+
+    @BeforeEach
+    void setup() {
+        interceptor = new AuthorizationGecHeaderInterceptor();
+        template = new RequestTemplate();
+        user = mock(SelfCareUser.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(mock(Authentication.class));
+        SecurityContextHolder.setContext(securityContext);
+    }
+
+    @Test
+    void testApply() {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        RequestTemplate template = new RequestTemplate();
+
+        user = mock(SelfCareUser.class);
+
+        ReflectionTestUtils.setField(interceptor, "gecSubscriptionKey", "sub-key");
+        ReflectionTestUtils.setField(interceptor, "flagAuthorization", "true");
+
+        when(auth.getPrincipal()).thenReturn(user);
+        when(user.getId()).thenReturn("user-id");
+
+        interceptor.apply(template);
+    }
+
+
+}
