696cf2bedf609c3503167bd9550973513af93a96
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2023-09-22 08:19:51 +0200

    add test

--- core/src/test/java/it/pagopa/selfcare/pagopa/backoffice/core/GecServiceImplTest.java
+++ core/src/test/java/it/pagopa/selfcare/pagopa/backoffice/core/GecServiceImplTest.java
@@ -3,6 +3,7 @@ package it.pagopa.selfcare.pagopa.backoffice.core;
 import it.pagopa.selfcare.pagopa.backoffice.connector.api.GecConnector;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.channel.Channels;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.gec.Bundles;
+import it.pagopa.selfcare.pagopa.backoffice.connector.model.gec.Touchpoints;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,49 @@ class GecServiceImplTest {
         reflectionEqualsByName(bundlesMock, bundles);
         verify(gecConnectorMock, times(1))
                 .getBundlesByCI(cifiscalcode, limit, page, xRequestId);
+        verifyNoMoreInteractions(gecConnectorMock);
+    }
+
+    @Test
+    void getTouchpoints() {
+        //given
+        final Integer limit = 1;
+        final Integer page = 1;
+        final String xRequestId = "xRequestId";
+
+        Touchpoints touchpointsMock = mock(Touchpoints.class);
+        when(gecConnectorMock.getTouchpoints( any(), any(), any()))
+                .thenReturn(touchpointsMock);
+        //when
+        Touchpoints touchpoints = gecService.getTouchpoints(limit, page, xRequestId);
+        //then
+        assertNotNull(touchpoints);
+        assertEquals(touchpointsMock, touchpoints);
+        reflectionEqualsByName(touchpointsMock, touchpoints);
+        verify(gecConnectorMock, times(1))
+                .getTouchpoints(limit, page, xRequestId);
+        verifyNoMoreInteractions(gecConnectorMock);
+    }
+
+    @Test
+    void getBundlesByPSP() {
+        //given
+        final Integer limit = 1;
+        final Integer page = 1;
+        final String pspcode = "pspcode";
+
+        final String xRequestId = "xRequestId";
+        Bundles bundlesMock = mock(Bundles.class);
+        when(gecConnectorMock.getBundlesByPSP(any(), any(), any(), any()))
+                .thenReturn(bundlesMock);
+        //when
+        Bundles bundles = gecService.getBundlesByPSP(pspcode, limit, page, xRequestId);
+        //then
+        assertNotNull(bundles);
+        assertEquals(bundlesMock, bundles);
+        reflectionEqualsByName(bundlesMock, bundles);
+        verify(gecConnectorMock, times(1))
+                .getBundlesByPSP(pspcode, limit, page, xRequestId);
         verifyNoMoreInteractions(gecConnectorMock);
     }
 }
