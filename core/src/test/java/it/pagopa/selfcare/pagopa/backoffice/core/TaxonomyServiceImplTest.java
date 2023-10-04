eda50ed93ee222ccc4f166da9ade7d440345e3c3
Biagio Murano <biagio.murano@emea.nttdata.com>
2023-10-04 10:32:40 +0200

    swagger placeholder

--- core/src/test/java/it/pagopa/selfcare/pagopa/backoffice/core/TaxonomyServiceImplTest.java
+++ core/src/test/java/it/pagopa/selfcare/pagopa/backoffice/core/TaxonomyServiceImplTest.java
@@ -1,44 +1,19 @@
 package it.pagopa.selfcare.pagopa.backoffice.core;
 
-import it.pagopa.selfcare.pagopa.backoffice.connector.api.ApiConfigConnector;
 import it.pagopa.selfcare.pagopa.backoffice.connector.api.TaxonomyConnector;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.PageInfo;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.broker.BrokerDetails;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.broker.Brokers;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.channel.*;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.creditorInstitution.*;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.CreditorInstitutionStationEdit;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.Station;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.StationDetails;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.Stations;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.taxonomy.Taxonomies;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.taxonomy.Taxonomy;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.wrapper.WrapperChannel;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.wrapper.WrapperChannels;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.wrapper.WrapperStation;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.wrapper.WrapperStations;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static it.pagopa.selfcare.pagopa.TestUtils.mockInstance;
 import static it.pagopa.selfcare.pagopa.TestUtils.reflectionEqualsByName;
-import static it.pagopa.selfcare.pagopa.backoffice.core.ApiConfigServiceImpl.CREDITOR_INSTITUTION_CODE_IS_REQUIRED;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
