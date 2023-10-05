b1d2d4628e9f027401f6de5b9c65e3c829a98b7e
Biagio Murano <biagio.murano@emeal.nttadata.com>
2023-10-05 15:03:36 +0200

    delete import

--- connector/rest/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/client/TaxonomyRestClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/rest/client/TaxonomyRestClient.java
@@ -1,7 +1,6 @@
 package it.pagopa.selfcare.pagopa.backoffice.connector.rest.client;
 
 import it.pagopa.selfcare.pagopa.backoffice.connector.api.TaxonomyConnector;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.taxonomy.Taxonomies;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.taxonomy.Taxonomy;
 import it.pagopa.selfcare.pagopa.backoffice.connector.rest.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
