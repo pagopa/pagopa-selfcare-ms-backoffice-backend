60cd7d46a41d6d8d2e612d6ac522b1cd37e49bb3
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2023-10-06 15:08:46 +0200

    remove unused import

--- connector-api/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/model/gec/BundlePaymentTypes.java
+++ connector-api/src/main/java/it/pagopa/selfcare/pagopa/backoffice/connector/model/gec/BundlePaymentTypes.java
@@ -3,6 +3,7 @@ package it.pagopa.selfcare.pagopa.backoffice.connector.model.gec;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.PageInfo;
 import lombok.Data;
+
 import java.util.List;
 
 @Data
