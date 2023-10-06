52053fc8c8574a05d908924ad6ab03ea9e75442c
Biagio Murano <biagio.murano@emeal.nttadata.com>
2023-10-06 14:10:57 +0200

    wip cache

--- web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/controller/TaxonomyController.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/backoffice/web/controller/TaxonomyController.java
@@ -2,20 +2,18 @@ package it.pagopa.selfcare.pagopa.backoffice.web.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.station.Stations;
-import it.pagopa.selfcare.pagopa.backoffice.connector.model.taxonomy.Taxonomies;
 import it.pagopa.selfcare.pagopa.backoffice.connector.model.taxonomy.Taxonomy;
 import it.pagopa.selfcare.pagopa.backoffice.core.TaxonomyService;
 import it.pagopa.selfcare.pagopa.backoffice.web.model.mapper.TaxonomyMapper;
-import it.pagopa.selfcare.pagopa.backoffice.web.model.stations.StationsResource;
-import it.pagopa.selfcare.pagopa.backoffice.web.model.taxonomies.TaxonomiesResource;
 import it.pagopa.selfcare.pagopa.backoffice.web.model.taxonomies.TaxonomyResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -37,9 +35,10 @@ public class TaxonomyController {
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "", notes = "${swagger.api.taxonomy.getTaxonomy}")
-    public  List<TaxonomyResource> getTaxonomy() {
+    public List<TaxonomyResource> getTaxonomy() {
         List<Taxonomy> taxonomies = taxonomyService.getTaxonomies();
         List<TaxonomyResource> resource = TaxonomyMapper.toResource(taxonomies);
         return resource;
     }
+
 }
