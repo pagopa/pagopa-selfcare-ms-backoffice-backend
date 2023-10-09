400dc3ee54be3d2913a78a7784dd32998bc155c9
Jacopo <jacopo.carlini@gmail.com>
2023-10-09 17:10:54 +0200

    [PAGOPA-1215] logging

--- app/src/main/java/it/pagopa/selfcare/pagopa/backoffice/config/LoggingAspect.java
+++ app/src/main/java/it/pagopa/selfcare/pagopa/backoffice/config/LoggingAspect.java
@@ -3,7 +3,10 @@ package it.pagopa.selfcare.pagopa.backoffice.config;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -63,7 +66,9 @@ public class LoggingAspect {
         // all service methods
     }
 
-    /** Log essential info of application during the startup. */
+    /**
+     * Log essential info of application during the startup.
+     */
     @PostConstruct
     public void logStartup() {
         log.info("-> Starting {} version {} - environment {}", name, version, environment);
@@ -90,51 +95,47 @@ public class LoggingAspect {
                                         || prop.toLowerCase().contains("password")
                                         || prop.toLowerCase().contains("pass")
                                         || prop.toLowerCase().contains("pwd")
-                                        || prop.toLowerCase().contains("key") || prop.toLowerCase().contains("secret")))
+                                        || prop.toLowerCase().contains("key")
+                                        || prop.toLowerCase().contains("secret")
+                                ))
                 .forEach(prop -> log.debug("{}: {}", prop, env.getProperty(prop)));
     }
 
     @Around(value = "restController()")
-    public Object logService(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object logApiInvocation(ProceedingJoinPoint joinPoint) throws Throwable {
         MDC.put(METHOD, joinPoint.getSignature().getName());
         MDC.put(START_TIME, String.valueOf(System.currentTimeMillis()));
         log.info("{} {}", httRequest.getMethod(), httRequest.getRequestURI());
         log.info("Invoking API operation {} - args: {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+
         Object result = joinPoint.proceed();
+
         MDC.put(STATUS, "OK");
         MDC.put(CODE, String.valueOf(httpResponse.getStatus()));
         MDC.put(RESPONSE_TIME, getExecutionTime());
         log.info("Successful API operation {} - result: {}", joinPoint.getSignature().getName(), result);
+        MDC.remove(STATUS);
+        MDC.remove(CODE);
+        MDC.remove(RESPONSE_TIME);
+        MDC.remove(START_TIME);
         return result;
     }
 
-    @AfterReturning(
-            value = "@within(org.springframework.web.bind.annotation.RestControllerAdvice)",
-            returning = "result"
-    )
+    @AfterReturning(value = "execution(* *..exception.ErrorHandler.*(..))", returning = "result")
     public void trowingApiInvocation(JoinPoint joinPoint, ResponseEntity<?> result) {
         MDC.put(STATUS, "KO");
         MDC.put(CODE, String.valueOf(result.getStatusCodeValue()));
         MDC.put(RESPONSE_TIME, getExecutionTime());
-        log.info("Failed API operation {} - error: {}", joinPoint.getSignature().getName(), result);
-    }
-
-    @Around(value = "repository() || service()")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        long startTime = System.currentTimeMillis();
-        Object result = joinPoint.proceed();
-        long endTime = System.currentTimeMillis();
-        log.trace(
-                "Time taken for Execution of {} is: {}ms",
-                joinPoint.getSignature().toShortString(),
-                (endTime - startTime));
-        return result;
+        log.info("Failed API operation {} - error: {}", MDC.get(METHOD), result);
+        MDC.remove(STATUS);
+        MDC.remove(CODE);
+        MDC.remove(RESPONSE_TIME);
+        MDC.remove(START_TIME);
     }
 
     @Around(value = "repository() || service()")
     public Object logTrace(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.debug(
-                "Call method {} - args: {}", joinPoint.getSignature().toShortString(), joinPoint.getArgs());
+        log.debug("Call method {} - args: {}", joinPoint.getSignature().toShortString(), joinPoint.getArgs());
         Object result = joinPoint.proceed();
         log.debug("Return method {} - result: {}", joinPoint.getSignature().toShortString(), result);
         return result;
