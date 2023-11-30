d132d1c506d502cdf7415656722376f167722463
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-11-30 09:41:22 +0100

    fix: removed mandatory constraint for address fields

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/creditorinstituions/CreditorInstitutionAddressResource.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/creditorinstituions/CreditorInstitutionAddressResource.java
@@ -5,16 +5,16 @@ import lombok.Data;
 
 @Data
 public class CreditorInstitutionAddressResource {
-    @ApiModelProperty(value = "Creditor Institution's physical address", required = true)
+    @ApiModelProperty(value = "Creditor Institution's physical address")
     private String location;
 
-    @ApiModelProperty(value = "Creditor Institution's city", required = true)
+    @ApiModelProperty(value = "Creditor Institution's city")
     private String city;
 
-    @ApiModelProperty(value = "Creditor Institution's zip code", required = true)
+    @ApiModelProperty(value = "Creditor Institution's zip code")
     private String zipCode;
 
-    @ApiModelProperty(value = "Creditor Institution's country code", required = true)
+    @ApiModelProperty(value = "Creditor Institution's country code")
     private String countryCode;
 
     @ApiModelProperty(value = "Creditor Institution's tax domicile")
