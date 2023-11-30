7944f6516c7a70ba3525dab08d62fc79e03b4096
Jacopo <jacopo.carlini@gmail.com>
2023-11-30 11:52:13 +0100

    get info

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/BaseController.java
@@ -0,0 +1,78 @@
+package it.pagopa.selfcare.pagopa.backoffice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.pagopa.selfcare.pagopa.backoffice.model.AppInfo;
+import it.pagopa.selfcare.pagopa.backoffice.model.Problem;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class BaseController {
+  @Value("${info.application.name}")
+  private String name;
+
+  @Value("${info.application.version}")
+  private String version;
+
+  @Value("${info.properties.environment}")
+  private String environment;
+
+  @Operation(
+      summary = "health check",
+      description = "Return OK if application is started",
+      security = {@SecurityRequirement(name = "ApiKey")},
+      tags = {"Home"})
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "OK",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = AppInfo.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad Request",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(
+            responseCode = "403",
+            description = "Forbidden",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(
+            responseCode = "429",
+            description = "Too many requests",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Service unavailable",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Problem.class)))
+      })
+  @GetMapping(value = "/info")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<AppInfo> healthCheck() {
+    // Used just for health checking
+    AppInfo info = AppInfo.builder().name(name).version(version).environment(environment).build();
+    return ResponseEntity.status(HttpStatus.OK).body(info);
+  }
+}
