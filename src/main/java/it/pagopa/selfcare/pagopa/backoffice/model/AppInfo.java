74e39d4cf3710f2ddd84d605f58fb34055a1a225
Jacopo <jacopo.carlini@gmail.com>
2023-11-30 11:52:13 +0100

    get info

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/AppInfo.java
@@ -0,0 +1,17 @@
+package it.pagopa.selfcare.pagopa.backoffice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AppInfo {
+
+  private String name;
+  private String version;
+  private String environment;
+}
