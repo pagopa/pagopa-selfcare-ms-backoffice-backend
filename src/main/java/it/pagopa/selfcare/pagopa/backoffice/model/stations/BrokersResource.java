649c83a3fcb606a0035642b25864d34c2b747574
Jacopo <jacopo.carlini@gmail.com>
2023-12-05 11:14:24 +0100

    get brokers

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/stations/BrokersResource.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/stations/BrokersResource.java
@@ -1,11 +1,16 @@
 package it.pagopa.selfcare.pagopa.backoffice.model.stations;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import it.pagopa.selfcare.pagopa.backoffice.model.connector.PageInfo;
 import lombok.Data;
 
 import java.util.List;
+
 @Data
 public class BrokersResource {
     @JsonProperty("brokers")
     private List<BrokerResource> brokerList;
+
+    @JsonProperty("page_info")
+    private PageInfo pageInfo;
 }
