95248254409add998dfcf0164e8abf4b191a906e
Jacopo <jacopo.carlini@gmail.com>
2024-01-05 14:01:07 +0100

    [PASELC-643] apikey FDR

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/Subscription.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/Subscription.java
@@ -1,32 +1,22 @@
 package it.pagopa.selfcare.pagopa.backoffice.model.institutions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Subscription {
 
     NODOAUTH("/products/nodo-auth", "Connessione con nodo", "nodauth-"),
-    GPD("/products/debt-positions", "Integrazione Asincrona - Posizioni Debitorie", "gdp-"),
     BIZ("/products/bizevents", "Recupero Ricevuta", "biz-"),
+    GPD("/products/debt-positions", "Integrazione Asincrona - Posizioni Debitorie", "gdp-"),
     GPD_REP("/products/product-gpd-reporting", "Integrazione Asincrona - Gestione flussi di rendicontazione", "gpdrep-"),
-    GPD_PAY("/products/gpd-payments-rest-aks", "Integrazione Asincrona - Ricevute", "gpdpay-");
+    GPD_PAY("/products/gpd-payments-rest-aks", "Integrazione Asincrona - Ricevute", "gpdpay-"),
+    FDR_ORG("/products/fdr-org", "FdR [ORG]", "fdrorg-"),
+    FDR_PSP("/products/fdr-psp", "FdR [PSP]", "fdrpsp-");
 
     private final String scope;
     private final String displayName;
     private final String prefixId;
 
-    Subscription(String scope, String displayName, String prefixId) {
-        this.scope = scope;
-        this.displayName = displayName;
-        this.prefixId = prefixId;
-    }
-
-    public String getPrefixId() {
-        return prefixId;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public String getScope() {
-        return scope;
-    }
 }
