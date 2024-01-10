66101e147be46d1caed5d8561fd47c776c733f0a
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-01-10 13:04:53 +0100

    feat: added entities for handle broker IBANs in 'Limbo DB'

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/entity/BrokerIbanEntity.java
@@ -0,0 +1,23 @@
+package it.pagopa.selfcare.pagopa.backoffice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BrokerIbanEntity {
+
+    private String ciName;
+    private String ciFiscalCode;
+    private String iban;
+    private String status;
+    private OffsetDateTime validityDate;
+    private String description;
+    private String label;
+}
