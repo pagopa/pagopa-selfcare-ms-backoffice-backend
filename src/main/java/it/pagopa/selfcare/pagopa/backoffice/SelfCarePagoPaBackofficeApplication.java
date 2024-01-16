5e85cffe2733028e90702146f6df169670cb30b8
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-01-16 09:43:10 +0100

    fix: added missing EnableRetry activation for Feign client

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/SelfCarePagoPaBackofficeApplication.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/SelfCarePagoPaBackofficeApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients
+@EnableRetry
 public class SelfCarePagoPaBackofficeApplication {
 
     public static void main(String[] args) {
