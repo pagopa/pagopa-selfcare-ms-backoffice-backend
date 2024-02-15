3a4885aec57e02888737455d75156bac3479d92a
svariant <samuele.varianti@nttdata.com>
2024-02-15 14:01:49 +0100

    fix: Fixed sonar smell with class constructor

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/commissionbundle/client/BundleCreateResponse.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/commissionbundle/client/BundleCreateResponse.java
@@ -2,4 +2,8 @@ package it.pagopa.selfcare.pagopa.backoffice.model.commissionbundle.client;
 
 public class BundleCreateResponse {
     private String idBundle;
+
+    public BundleCreateResponse(String idBundle) {
+        this.idBundle = idBundle;
+    }
 }
