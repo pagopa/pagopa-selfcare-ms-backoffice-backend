2077969f267ac69795ac6e7f366f1c6481103624
Jacopo <jacopo.carlini@gmail.com>
2024-02-19 13:33:15 +0100

    hotfix getDelegation

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/RoleType.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/RoleType.java
@@ -1,9 +1,28 @@
 package it.pagopa.selfcare.pagopa.backoffice.model.institutions;
 
+import it.pagopa.selfcare.pagopa.backoffice.exception.AppError;
+import it.pagopa.selfcare.pagopa.backoffice.exception.AppException;
+import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
 public enum RoleType {
 
-    EC,
-    PSP,
-    PT
+    EC("Ente Creditore", List.of("PA", "GSP", "SCP", "PG")),
+    PSP("Payment Service Provider", List.of("PSP")),
+    PT("Partner Tecnologico", List.of("PT"));
+
+    private final String value;
+    private final List<String> selfcareRole;
+
+
+    public static RoleType fromSelfcareRole(String role) {
+        return Arrays.stream(RoleType.values())
+                .filter(elem -> elem.selfcareRole.contains(role))
+                .findFirst()
+                .orElseThrow(() -> new AppException(AppError.SELFCARE_ROLE_NOT_FOUND, role));
+    }
 
 }
