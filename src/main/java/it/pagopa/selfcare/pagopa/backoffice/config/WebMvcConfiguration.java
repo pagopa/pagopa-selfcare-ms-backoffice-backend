a4f886865be1b71c58879f93f554e0a0d4f841f4
Jacopo <jacopo.carlini@gmail.com>
2024-02-21 14:35:32 +0100

    new pattern

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/config/WebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package it.pagopa.selfcare.pagopa.backoffice.config;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @SneakyThrows
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
