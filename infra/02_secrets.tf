ad2517ba0f12f833e04f9e3000ef4f16227c20ec
Jacopo <jacopo.carlini@gmail.com>
2024-02-21 10:13:58 +0100

    [VAS-710] sops secret

---
+++ infra/02_secrets.tf
@@ -0,0 +1,18 @@
+data "azurerm_key_vault" "kv" {
+  name                = "${local.product}-${local.domain}-kv"
+  resource_group_name = "${local.product}-${local.domain}-sec-rg"
+}
+
+
+resource "azurerm_key_vault_key" "sops_key" {
+  count        = var.env_short == "d" ? 1 : 0
+  name         = "${local.service}-sops-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+  ]
+}
