1c091b192215980e06698769eef262530bf95187
svariant <128470180+svariant@users.noreply.github.com>
2024-02-23 09:30:15 +0100

    [VAS-741] feat: Implemented afm-marketplace APIs to manage bundles (#318)
    Co-authored-by: Jacopo Carlini <jacopo.carlini@gmail.com>

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/commissionbundle/Bundle.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/commissionbundle/Bundle.java
@@ -1,5 +1,6 @@
 package it.pagopa.selfcare.pagopa.backoffice.model.commissionbundle;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import it.pagopa.selfcare.pagopa.backoffice.model.commissionbundle.client.BundleType;
 import lombok.AllArgsConstructor;
@@ -17,36 +18,39 @@ import java.util.List;
 @NoArgsConstructor
 public class Bundle {
 
-    private String idBundle;
-
-    private String idCiBundle;
-
-    private String idPsp;
-
+    @JsonProperty("idBundle")
+    @Schema(description = "Document ID")
+    private String id;
     private String name;
-
     private String description;
-
-    @Schema(description = "The fees of this bundle")
-    private Integer paymentAmount;
-
-    private Integer minPaymentAmount;
-
-    private Integer maxPaymentAmount;
-
+    @Schema(description = "Applied commission fee")
+    private Long paymentAmount;
+    @Schema(description = "Minimum commission fee that can be applied")
+    private Long minPaymentAmount;
+    @Schema(description = "Maximum commission fee that can bee applied")
+    private Long maxPaymentAmount;
+    @Schema(description = "Payment type for which the bundle can be applied")
     private String paymentType;
-
+    @Schema(description = "Touch-point where the bundle can be applied")
     private String touchpoint;
-
+    @Schema(description = "Bundle enum type")
     private BundleType type;
-
+    @Schema(description = "List of taxonomies that relates to the bundle")
     private List<String> transferCategoryList;
-
+    @Schema(description = "Date from which the bundle is valid")
     private LocalDate validityDateFrom;
-
+    @Schema(description = "Date after which the bundle is expired")
     private LocalDate validityDateTo;
-
+    @Schema(description = "Date of creation")
     private LocalDateTime insertedDate;
-
+    @Schema(description = "Date of the last change")
     private LocalDateTime lastUpdatedDate;
+    @Schema(description = "ID of the linked channel")
+    private String idChannel;
+    @Schema(description = "TaxCode of the linked broker PSP")
+    private String idBrokerPsp;
+    @Schema(description = "If the payment needs a tax stamp, mutually exclusive with digitalStampRestriction")
+    private Boolean digitalStamp;
+    @Schema(description = "If the payment can be payed only with a tax stamp, mutually exclusive with digitalStamp")
+    private Boolean digitalStampRestriction;
 }
