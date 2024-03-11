ad1e194e6c0cdb082e9bffaaa248bdb906702380
Jacopo <jacopo.carlini@gmail.com>
2024-03-11 15:09:46 +0100

    feature

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/BaseController.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/BaseController.java
@@ -1,9 +1,12 @@
 package it.pagopa.selfcare.pagopa.backoffice.controller;
 
+import com.azure.spring.cloud.feature.management.FeatureManager;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import it.pagopa.selfcare.pagopa.backoffice.model.AppInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-public class BaseController {
+@Configuration
+@Slf4j
+public class BaseController  {
     @Value("${info.application.name}")
     private String name;
 
@@ -21,10 +26,20 @@ public class BaseController {
     @Value("${info.properties.environment}")
     private String environment;
 
+    private final FeatureManager featureManager;
+
+    public BaseController(FeatureManager featureManager) {
+        this.featureManager = featureManager;
+    }
+
+
     @Operation(summary = "health check", description = "Return OK if application is started", security = {@SecurityRequirement(name = "JWT")}, tags = {"Home"})
     @GetMapping(value = "/info")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<AppInfo> healthCheck() {
+        log.info(featureManager.getAllFeatureNames().toString());
+        log.info(featureManager.isEnabledAsync("Beta").block().toString());
+        log.info(featureManager.isEnabledAsync("test").block().toString());
         // Used just for health checking
         AppInfo info = AppInfo.builder().name(name).version(version).environment(environment).build();
         return ResponseEntity.status(HttpStatus.OK).body(info);
