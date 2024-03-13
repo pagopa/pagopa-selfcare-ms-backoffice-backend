30cd53872f0ff7ac026e67a29083002bbf7e5a8e
Jacopo Carlini <jacopo.carlini@gmail.com>
2024-03-13 17:36:29 +0100

    Feature flags (#334)

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/config/azure/FeatureFilterConfigurations.java
@@ -0,0 +1,45 @@
+package it.pagopa.selfcare.pagopa.backoffice.config.azure;
+
+import com.azure.spring.cloud.feature.management.filters.PercentageFilter;
+import com.azure.spring.cloud.feature.management.filters.TargetingFilter;
+import com.azure.spring.cloud.feature.management.filters.TimeWindowFilter;
+import com.azure.spring.cloud.feature.management.targeting.TargetingContextAccessor;
+import com.azure.spring.cloud.feature.management.targeting.TargetingEvaluationOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeatureFilterConfigurations {
+
+    private final OperationFeatureFilter operationFeatureFilter;
+
+    public FeatureFilterConfigurations(OperationFeatureFilter operationFeatureFilter) {
+        this.operationFeatureFilter = operationFeatureFilter;
+    }
+
+    @Bean
+    public PercentageFilter percentageFilter() {
+        return new PercentageFilter();
+    }
+
+    @Bean
+    public TimeWindowFilter timeWindowFilter() {
+        return new TimeWindowFilter();
+    }
+
+    @Bean(name = "Operation")
+    public OperationFeatureFilter operation() {
+        return operationFeatureFilter;
+    }
+
+    @Bean(name = "Microsoft.Targeting")
+    public TargetingFilter targetingFilter(TargetingContextAccessor contextAccessor) {
+        return new TargetingFilter(contextAccessor, new TargetingEvaluationOptions().setIgnoreCase(true));
+    }
+
+    @Bean
+    public TargetingContextAccessor targetingContextAccessor() {
+        return new TargetingContextAccessorImpl();
+    }
+
+}
