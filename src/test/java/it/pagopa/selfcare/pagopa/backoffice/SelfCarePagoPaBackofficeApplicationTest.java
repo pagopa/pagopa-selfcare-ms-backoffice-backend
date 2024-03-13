27ca46a21a3379455b6d8ce229c07928cfc7b173
Jacopo <jacopo.carlini@gmail.com>
2024-03-13 17:28:15 +0100

    fix dep

---
+++ src/test/java/it/pagopa/selfcare/pagopa/backoffice/SelfCarePagoPaBackofficeApplicationTest.java
@@ -0,0 +1,13 @@
+package it.pagopa.selfcare.pagopa.backoffice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SelfCarePagoPaBackofficeApplicationTest {
+
+    @Test
+    void applicationContextLoaded() {
+        assertTrue(true); // it just tests that an error has not occurred
+    }
+}
