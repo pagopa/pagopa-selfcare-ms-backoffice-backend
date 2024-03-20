c31adc6a9fa8d5d56c699ebdcfe917e7a952e61a
Jacopo Carlini <jacopo.carlini@gmail.com>
2024-03-20 17:34:35 +0100

    [VAS-868] fix psp create/update (#346)

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/config/azure/FeatureFilterConfigurations.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/config/azure/FeatureFilterConfigurations.java
@@ -5,27 +5,11 @@ import com.azure.spring.cloud.feature.management.filters.TargetingFilter;
 import com.azure.spring.cloud.feature.management.filters.TimeWindowFilter;
 import com.azure.spring.cloud.feature.management.targeting.TargetingContextAccessor;
 import com.azure.spring.cloud.feature.management.targeting.TargetingEvaluationOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeatureFilterConfigurations {
-
-    @Value("${azure.resource-manager.api-manager.client-id}")
-    private String clientId;
-
-    @Value("${azure.resource-manager.api-manager.client-secret}")
-    private String clientSecret;
-
-    @Value("${azure.resource-manager.api-manager.tenant-id}")
-    private String tenantId;
-
-    private final OperationFeatureFilter operationFeatureFilter;
-
-    public FeatureFilterConfigurations(OperationFeatureFilter operationFeatureFilter) {
-        this.operationFeatureFilter = operationFeatureFilter;
-    }
 
     @Bean
     public PercentageFilter percentageFilter() {
@@ -35,11 +19,6 @@ public class FeatureFilterConfigurations {
     @Bean
     public TimeWindowFilter timeWindowFilter() {
         return new TimeWindowFilter();
-    }
-
-    @Bean(name = "Operation")
-    public OperationFeatureFilter operation() {
-        return operationFeatureFilter;
     }
 
     @Bean(name = "Microsoft.Targeting")
--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/connector/channel/PaymentServiceProvider.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/connector/channel/PaymentServiceProvider.java
@@ -2,11 +2,17 @@ package it.pagopa.selfcare.pagopa.backoffice.model.connector.channel;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentServiceProvider {
 
     @JsonProperty("psp_code")
