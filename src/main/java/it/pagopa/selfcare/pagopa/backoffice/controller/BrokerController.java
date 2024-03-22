46624264fa844e1d401cab7b936bc39f6170c126
giomella <gioele.mella@emeal.nttdata.com>
2024-03-22 10:29:09 +0100

    [VAS-689] added new api for retrieve the information about ci brokers delegations

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/BrokerController.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/BrokerController.java
@@ -6,7 +6,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.BrokerEcDto;
 import it.pagopa.selfcare.pagopa.backoffice.model.export.BrokerECExportStatus;
-import it.pagopa.selfcare.pagopa.backoffice.model.stations.*;
+import it.pagopa.selfcare.pagopa.backoffice.model.institutions.MyCIResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.stations.BrokerDetailsResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.stations.BrokerDto;
+import it.pagopa.selfcare.pagopa.backoffice.model.stations.BrokerResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.stations.BrokersResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.stations.StationDetailsResourceList;
 import it.pagopa.selfcare.pagopa.backoffice.service.BrokerService;
 import it.pagopa.selfcare.pagopa.backoffice.service.ExportService;
 import it.pagopa.selfcare.pagopa.backoffice.util.OpenApiTableMetadata;
@@ -19,9 +24,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -50,46 +64,58 @@ public class BrokerController {
 
     @GetMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get paginated list of creditor brokers", security = {@SecurityRequirement(name = "JWT")})
+    @Operation(summary = "Get paginated list of creditor brokers",
+            security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ)
-    public BrokersResource getBrokersEC(@Parameter(description = "") @RequestParam(required = false, defaultValue = "50") Integer limit,
-                                        @Parameter(description = "Page number. Page value starts from 0") @RequestParam Integer page,
-                                        @RequestParam(required = false) String code,
-                                        @RequestParam(required = false) String name,
-                                        @Parameter(description = "order by name or code, default = CODE") @RequestParam(required = false, defaultValue = "CODE") String orderby,
-                                        @Parameter() @RequestParam(required = false, defaultValue = "DESC") String ordering) {
+    public BrokersResource getBrokersEC(
+            @Parameter(description = "") @RequestParam(required = false,
+                    defaultValue = "50") Integer limit,
+            @Parameter(
+                    description = "Page number. Page value starts from 0") @RequestParam Integer page,
+            @RequestParam(required = false) String code,
+            @RequestParam(required = false) String name,
+            @Parameter(description = "order by name or code, default = CODE") @RequestParam(
+                    required = false, defaultValue = "CODE") String orderby,
+            @Parameter() @RequestParam(required = false, defaultValue = "DESC") String ordering) {
         return brokerService.getBrokersEC(limit, page, code, name, orderby, ordering);
     }
 
 
     @PutMapping(value = "/{broker-code}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Update an existing EC broker", security = {@SecurityRequirement(name = "JWT")})
+    @Operation(summary = "Update an existing EC broker",
+            security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.WRITE)
-    public BrokerDetailsResource updateBroker(@RequestBody @Valid BrokerEcDto dto,
-                                              @Parameter(description = "Broker code") @PathVariable("broker-code") String brokerCode) {
+    public BrokerDetailsResource updateBroker(@RequestBody @Valid BrokerEcDto dto, @Parameter(
+            description = "Broker code") @PathVariable("broker-code") String brokerCode) {
 
         return brokerService.updateBrokerForCI(dto, brokerCode);
     }
 
     @GetMapping(value = "/{broker-code}/stations", produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get paginated list of stations given a broker code", security = {@SecurityRequirement(name = "JWT")})
+    @Operation(summary = "Get paginated list of stations given a broker code",
+            security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ)
-    public StationDetailsResourceList getStationsDetailsListByBroker(@PathVariable("broker-code") String brokerCode,
-                                                                     @RequestParam(required = false) String stationId,
-                                                                     @RequestParam(required = false, defaultValue = "10") Integer limit,
-                                                                     @RequestParam(required = false, defaultValue = "0") Integer page) {
+    public StationDetailsResourceList getStationsDetailsListByBroker(
+            @PathVariable("broker-code") String brokerCode,
+            @RequestParam(required = false) String stationId,
+            @RequestParam(required = false, defaultValue = "10") Integer limit,
+            @RequestParam(required = false, defaultValue = "0") Integer page) {
         return brokerService.getStationsDetailsListByBroker(brokerCode, stationId, limit, page);
     }
 
-    @GetMapping(value = "/{broker-code}/ibans/export", produces = {"text/csv", MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/{broker-code}/ibans/export",
+            produces = {"text/csv", MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Export all IBANs of all creditor institutions handled by a broker EC to CSV", security = {@SecurityRequirement(name = "JWT")},
+    @Operation(
+            summary = "Export all IBANs of all creditor institutions handled by a broker EC to CSV",
+            security = {@SecurityRequirement(name = "JWT")},
             description = "The CSV file contains the following columns: `denominazioneEnte, codiceFiscale, iban, stato, dataAttivazioneIban, descrizione, etichetta`")
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ, cacheable = true)
     @Cacheable(value = "exportIbansToCsv")
-    public ResponseEntity<Resource> exportIbansToCsv(@Parameter(description = "SelfCare Broker Code. it's a tax code") @PathVariable("broker-code") String brokerCode) {
+    public ResponseEntity<Resource> exportIbansToCsv(@Parameter(
+            description = "SelfCare Broker Code. it's a tax code") @PathVariable("broker-code") String brokerCode) {
 
         byte[] file = exportService.exportIbansToCsv(brokerCode);
 
@@ -99,13 +125,16 @@ public class BrokerController {
                 .body(new ByteArrayResource(file));
     }
 
-    @GetMapping(value = "/{broker-code}/creditor-institutions/export", produces = {"text/csv", MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/{broker-code}/creditor-institutions/export",
+            produces = {"text/csv", MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Export all creditor institutions handled by a broker EC to CSV", security = {@SecurityRequirement(name = "JWT")},
+    @Operation(summary = "Export all creditor institutions handled by a broker EC to CSV",
+            security = {@SecurityRequirement(name = "JWT")},
             description = "The CSV file contains the following columns: `companyName, taxCode, intermediated, brokerCompanyName, brokerTaxCode, model, auxDigit, segregationCode, applicationCode, cbillCode, stationId, stationState, activationDate, version, broadcast`")
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ, cacheable = true)
     @Cacheable(value = "exportCreditorInstitutionToCsv")
-    public ResponseEntity<Resource> exportCreditorInstitutionToCsv(@Parameter(description = "SelfCare Broker Code. it's a tax code") @PathVariable("broker-code") String brokerCode) {
+    public ResponseEntity<Resource> exportCreditorInstitutionToCsv(@Parameter(
+            description = "SelfCare Broker Code. it's a tax code") @PathVariable("broker-code") String brokerCode) {
 
         byte[] file = exportService.exportCreditorInstitutionToCsv(brokerCode);
 
@@ -115,12 +144,33 @@ public class BrokerController {
                 .body(new ByteArrayResource(file));
     }
 
-    @GetMapping(value = "/{broker-code}/export-status", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/{broker-code}/export-status",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get all info about data exports for the broker EC", security = {@SecurityRequirement(name = "JWT")})
+    @Operation(summary = "Get all info about data exports for the broker EC",
+            security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ)
-    public BrokerECExportStatus getBrokerExportStatus(@Parameter(description = "SelfCare Broker Code. it's a tax code") @PathVariable("broker-code") String brokerCode) {
+    public BrokerECExportStatus getBrokerExportStatus(@Parameter(
+            description = "SelfCare Broker Code. it's a tax code") @PathVariable("broker-code") String brokerCode) {
 
         return exportService.getBrokerExportStatus(brokerCode);
+    }
+
+    /**
+     * Retrieves the list of creditor institution's delegation for the specified broker
+     * 
+     * @param brokerId the broker identifier
+     * @return the list of broker's delegations
+     */
+    @GetMapping("/{broker-id}/delegations")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+            summary = "Retrieve all delegations for given ci broker",
+            security = {@SecurityRequirement(name = "JWT")})
+    @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ, cacheable = true)
+    @Cacheable(value = "getBrokerDelegation")
+    public List<MyCIResource> getBrokerDelegation(@Parameter(
+            description = "Broker's unique id") @PathVariable("broker-id") String brokerId) {
+        return this.brokerService.getBrokerDelegation(brokerId);
     }
 }
--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/service/BrokerService.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/service/BrokerService.java
@@ -2,14 +2,21 @@ package it.pagopa.selfcare.pagopa.backoffice.service;
 
 import it.pagopa.selfcare.pagopa.backoffice.client.ApiConfigClient;
 import it.pagopa.selfcare.pagopa.backoffice.client.ApiConfigSelfcareIntegrationClient;
+import it.pagopa.selfcare.pagopa.backoffice.client.ExternalApiClient;
 import it.pagopa.selfcare.pagopa.backoffice.mapper.BrokerMapper;
 import it.pagopa.selfcare.pagopa.backoffice.mapper.CreditorInstitutionMapper;
 import it.pagopa.selfcare.pagopa.backoffice.model.connector.broker.BrokerDetails;
 import it.pagopa.selfcare.pagopa.backoffice.model.connector.broker.Brokers;
+import it.pagopa.selfcare.pagopa.backoffice.model.connector.creditorInstitution.CreditorInstitutionDetails;
 import it.pagopa.selfcare.pagopa.backoffice.model.connector.station.StationDetailsList;
 import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.BrokerEcDto;
+import it.pagopa.selfcare.pagopa.backoffice.model.institutions.DelegationExternal;
+import it.pagopa.selfcare.pagopa.backoffice.model.institutions.MyCIResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.institutions.RoleType;
 import it.pagopa.selfcare.pagopa.backoffice.model.stations.*;
 import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+import java.util.Objects;
 import org.mapstruct.factory.Mappers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +26,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class BrokerService {
 
-    private final CreditorInstitutionMapper mapper = Mappers.getMapper(CreditorInstitutionMapper.class);
+    private final CreditorInstitutionMapper mapper =
+            Mappers.getMapper(CreditorInstitutionMapper.class);
+
+    private final ApiConfigClient apiConfigClient;
+
+    private final ApiConfigSelfcareIntegrationClient apiConfigSelfcareIntegrationClient;
+
+    private final ModelMapper modelMapper;
+
+    private final ExternalApiClient externalApiClient;
 
     @Autowired
-    private ApiConfigClient apiConfigClient;
-
-    @Autowired
-    private ApiConfigSelfcareIntegrationClient apiConfigSelfcareIntegrationClient;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
+    public BrokerService(ApiConfigClient apiConfigClient,
+            ApiConfigSelfcareIntegrationClient apiConfigSelfcareIntegrationClient,
+            ModelMapper modelMapper, ExternalApiClient externalApiClient) {
+        this.apiConfigClient = apiConfigClient;
+        this.apiConfigSelfcareIntegrationClient = apiConfigSelfcareIntegrationClient;
+        this.modelMapper = modelMapper;
+        this.externalApiClient = externalApiClient;
+    }
 
     public BrokerResource createBroker(BrokerDto brokerDto) {
         BrokerDetails broker = apiConfigClient.createBroker(BrokerMapper.fromDto(brokerDto));
@@ -43,14 +59,68 @@ public class BrokerService {
         return mapper.toResource(dto);
     }
 
-    public StationDetailsResourceList getStationsDetailsListByBroker(String brokerId, String stationId, Integer limit, Integer page) {
-        StationDetailsList response = apiConfigSelfcareIntegrationClient.getStationsDetailsListByBroker(brokerId, stationId, limit, page);
+    public StationDetailsResourceList getStationsDetailsListByBroker(String brokerId,
+            String stationId, Integer limit, Integer page) {
+        StationDetailsList response = apiConfigSelfcareIntegrationClient
+                .getStationsDetailsListByBroker(brokerId, stationId, limit, page);
         return modelMapper.map(response, StationDetailsResourceList.class);
     }
 
 
-    public BrokersResource getBrokersEC(Integer limit, Integer page, String code, String name, String orderby, String ordering) {
+    public BrokersResource getBrokersEC(Integer limit, Integer page, String code, String name,
+            String orderby, String ordering) {
         Brokers response = apiConfigClient.getBrokersEC(limit, page, code, name, orderby, ordering);
         return modelMapper.map(response, BrokersResource.class);
+    }
+
+
+    /**
+     * Retrieves the list of broker's creditor institution delegation for the specified broker enriched with:
+     * <ul>
+     * <li>the institution's station count
+     * <li>the institution's cbill code
+     * 
+     * @param brokerId the broker identifier
+     * @return the enriched list of broker's delegations
+     */
+    public List<MyCIResource> getBrokerDelegation(String brokerId) {
+        List<DelegationExternal> delegationResponse =
+                this.externalApiClient.getBrokerDelegation(null, brokerId, "prod-pagopa", "FULL");
+
+        List<MyCIResource> delegationList = delegationResponse.stream()
+                .map(elem -> modelMapper.map(elem, MyCIResource.class)).toList();
+
+        // filter by roles
+        delegationList = delegationList.parallelStream().filter(Objects::nonNull)
+                .filter(delegation -> RoleType.EC
+                        .equals(RoleType.fromSelfcareRole(delegation.getInstitutionType())))
+                .toList();
+
+        delegationList.forEach(delegation -> {
+            delegation.setInstitutionStationCount(
+                    getInstitutionsStationCount(delegation.getBrokerId()));
+            delegation.setCbillCode(getInstitutionCbillCode(delegation.getInstitutionTaxCode()));
+        });
+
+        return delegationList;
+    }
+
+    private Long getInstitutionsStationCount(String brokerId) {
+        // TODO filter stations by creditor institution tax code
+        StationDetailsList response = this.apiConfigSelfcareIntegrationClient
+                .getStationsDetailsListByBroker(brokerId, null, 10, 0);
+        if (response.getPageInfo() != null && response.getPageInfo().getTotalItems() != null) {
+            return response.getPageInfo().getTotalItems();
+        }
+        return 0L;
+    }
+
+    private String getInstitutionCbillCode(String institutionTaxCode) {
+        CreditorInstitutionDetails dto =
+                this.apiConfigClient.getCreditorInstitutionDetails(institutionTaxCode);
+        if (dto != null) {
+            return dto.getCbillCode();
+        }
+        return null;
     }
 }
