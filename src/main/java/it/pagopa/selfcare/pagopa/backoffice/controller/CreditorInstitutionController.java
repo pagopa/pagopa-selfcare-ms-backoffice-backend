aad62eab57282cb0e8d2fdf459be156f97bd62d3
giomella <gioele.mella@emeal.nttdata.com>
2024-04-05 10:15:46 +0200

    [VAS-691] update openapi

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/CreditorInstitutionController.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/CreditorInstitutionController.java
@@ -10,7 +10,14 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.pagopa.selfcare.pagopa.backoffice.model.ProblemJson;
 import it.pagopa.selfcare.pagopa.backoffice.model.connector.creditorInstitution.CreditorInstitutionAssociatedCodeList;
-import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.*;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionAndBrokerDto;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionContactsResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionDetailsResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionDto;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionStationDto;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionStationEditResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.CreditorInstitutionsResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.creditorinstituions.UpdateCreditorInstitutionDto;
 import it.pagopa.selfcare.pagopa.backoffice.model.stations.BrokerAndEcDetailsResource;
 import it.pagopa.selfcare.pagopa.backoffice.service.CreditorInstitutionService;
 import it.pagopa.selfcare.pagopa.backoffice.util.OpenApiTableMetadata;
@@ -18,7 +25,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -49,16 +65,18 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get paginated list of creditor institutions", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public CreditorInstitutionsResource getCreditorInstitutions(@Parameter(description = "Number of elements on one page. Default = 50") @RequestParam(required = false, defaultValue = "50") Integer limit,
-                                                                @Parameter(description = "Page number. Page value starts from 0") @RequestParam Integer page,
-                                                                @Parameter(description = "Creditor institution code") @RequestParam(required = false, value = "ci-code") @NotBlank String ciCode,
-                                                                @Parameter(description = "Creditor institution name") @RequestParam(required = false, value = "name") String name,
-                                                                @Parameter(description = "Sorting method for paginated result") @RequestParam(required = false, value = "sorting") String sorting) {
+    public CreditorInstitutionsResource getCreditorInstitutions(
+            @Parameter(description = "Number of elements on one page. Default = 50") @RequestParam(required = false, defaultValue = "50") Integer limit,
+            @Parameter(description = "Page number. Page value starts from 0") @RequestParam Integer page,
+            @Parameter(description = "Creditor institution's tax code") @RequestParam(required = false, value = "ci-tax-code") @NotBlank String ciTaxCode,
+            @Parameter(description = "Creditor institution's name") @RequestParam(required = false, value = "name") String name,
+            @Parameter(description = "Sorting method for paginated result") @RequestParam(required = false, value = "sorting") String sorting
+    ) {
 
-        return ciService.getCreditorInstitutions(limit, page, ciCode, name, sorting);
+        return ciService.getCreditorInstitutions(limit, page, ciTaxCode, name, sorting);
     }
 
-    @GetMapping(value = "/{ci-code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{ci-tax-code}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CreditorInstitutionDetailsResource.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
@@ -71,12 +89,13 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get the detail of specific creditor institution", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public CreditorInstitutionDetailsResource getCreditorInstitutionDetails(@Parameter(description = "Creditor institution code") @PathVariable("ci-code") @NotBlank String ciCode) {
-
-        return ciService.getCreditorInstitutionDetails(ciCode);
+    public CreditorInstitutionDetailsResource getCreditorInstitutionDetails(
+            @Parameter(description = "Creditor institution's tax code") @PathVariable("ci-tax-code") @NotBlank String ciTaxCode
+    ) {
+        return ciService.getCreditorInstitutionDetails(ciTaxCode);
     }
 
-    @GetMapping(value = "/{ci-code}/segregation-codes", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{ci-tax-code}/segregation-codes", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CreditorInstitutionAssociatedCodeList.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
@@ -89,12 +108,13 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get segregation code associations with creditor institution", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public CreditorInstitutionAssociatedCodeList getCreditorInstitutionSegregationcodes(@Parameter(description = "Creditor institution code") @PathVariable("ci-code") @NotBlank String ciCode) {
-
-        return ciService.getCreditorInstitutionSegregationcodes(ciCode);
+    public CreditorInstitutionAssociatedCodeList getCreditorInstitutionSegregationcodes(
+            @Parameter(description = "Creditor institution's tax code") @PathVariable("ci-tax-code") @NotBlank String ciTaxCode
+    ) {
+            return ciService.getCreditorInstitutionSegregationcodes(ciTaxCode);
     }
 
-    @PostMapping(value = "/{ci-code}/station", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @PostMapping(value = "/{ci-tax-code}/station", produces = {MediaType.APPLICATION_JSON_VALUE})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Created", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CreditorInstitutionStationEditResource.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
@@ -108,13 +128,15 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Creates the relationship between the created station and the creditorInstitution", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public CreditorInstitutionStationEditResource associateStationToCreditorInstitution(@Parameter(description = "Creditor institution code") @PathVariable("ci-code") String ciCode,
-                                                                                        @RequestBody @NotNull CreditorInstitutionStationDto dto) {
-        return ciService.associateStationToCreditorInstitution(ciCode, dto);
+    public CreditorInstitutionStationEditResource associateStationToCreditorInstitution(
+            @Parameter(description = "Creditor institution's tax code") @PathVariable("ci-tax-code") String ciTaxCode,
+            @RequestBody @NotNull CreditorInstitutionStationDto dto
+    ) {
+        return ciService.associateStationToCreditorInstitution(ciTaxCode, dto);
     }
 
 
-    @DeleteMapping(value = "/{ci-code}/station/{station-code}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @DeleteMapping(value = "/{ci-tax-code}/station/{station-code}", produces = {MediaType.APPLICATION_JSON_VALUE})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema())),
             @ApiResponse(responseCode = "204", description = "No content", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema())),
@@ -128,10 +150,11 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "delete the relationship between the created station and the creditorInstitution", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public void deleteCreditorInstitutionStationRelationship(@Parameter(description = "Creditor institution code") @PathVariable("ci-code") String ciCode,
-                                                             @Parameter(description = "Channlecode or StationCode") @PathVariable("station-code") String stationCode) {
-
-        ciService.deleteCreditorInstitutionStationRelationship(ciCode, stationCode);
+    public void deleteCreditorInstitutionStationRelationship(
+            @Parameter(description = "Creditor institution's code") @PathVariable("ci-tax-code") String ciTaxCode,
+            @Parameter(description = "Station's code") @PathVariable("station-code") String stationCode
+    ) {
+        ciService.deleteCreditorInstitutionStationRelationship(ciTaxCode, stationCode);
     }
 
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -152,7 +175,7 @@ public class CreditorInstitutionController {
         return ciService.createCreditorInstitution(dto);
     }
 
-    @PostMapping(value = "/{ci-code}/full", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{ci-tax-code}/full", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Created", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CreditorInstitutionDetailsResource.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
@@ -165,14 +188,15 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Create a new creditor institution that can intermediate as a broker", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public CreditorInstitutionDetailsResource createCreditorInstitutionAndBroker(@Parameter(description = "Creditor institution code") @PathVariable("ci-code") @NotBlank String ciCode,
-                                                                                 @RequestBody @NotNull CreditorInstitutionAndBrokerDto dto) {
-
+    public CreditorInstitutionDetailsResource createCreditorInstitutionAndBroker(
+            @Parameter(description = "Creditor institution's tax code") @PathVariable("ci-tax-code") @NotBlank String ciTaxCode,
+            @RequestBody @NotNull CreditorInstitutionAndBrokerDto dto
+    ) {
         return ciService.createCIAndBroker(dto);
     }
 
 
-    @PutMapping(value = "/{ci-code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/{ci-tax-code}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CreditorInstitutionDetailsResource.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
@@ -185,13 +209,14 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Update an existing creditor institution", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public CreditorInstitutionDetailsResource updateCreditorInstitutionDetails(@Parameter(description = "Creditor institution code") @PathVariable("ci-code") @NotBlank String ciCode,
-                                                                               @RequestBody @Valid UpdateCreditorInstitutionDto dto) {
-
-        return ciService.updateCreditorInstitutionDetails(ciCode, dto);
+    public CreditorInstitutionDetailsResource updateCreditorInstitutionDetails(
+            @Parameter(description = "Creditor institution's tax code") @PathVariable("ci-tax-code") @NotBlank String ciTaxCode,
+            @RequestBody @Valid UpdateCreditorInstitutionDto dto
+    ) {
+        return ciService.updateCreditorInstitutionDetails(ciTaxCode, dto);
     }
 
-    @GetMapping(value = "/{ci-code}/full", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/{ci-tax-code}/full", produces = {MediaType.APPLICATION_JSON_VALUE})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = BrokerAndEcDetailsResource.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
@@ -204,8 +229,9 @@ public class CreditorInstitutionController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get PSP broker details", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata
-    public BrokerAndEcDetailsResource getBrokerAndEcDetails(@Parameter(description = "Broker code of an EC") @PathVariable(required = true, name = "ci-code") String brokerEcCode) {
-
+    public BrokerAndEcDetailsResource getBrokerAndEcDetails(
+            @Parameter(description = "Broker's tax code of an EC") @PathVariable(name = "ci-tax-code") String brokerEcCode
+    ) {
         return ciService.getBrokerAndEcDetails(brokerEcCode);
 
     }
