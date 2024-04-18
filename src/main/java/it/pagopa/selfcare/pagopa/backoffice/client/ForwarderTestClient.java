ce98c9b7561aca6bc2a3291f392c9cd5d9c52730
Jacopo <jacopo.carlini@gmail.com>
2024-04-18 09:51:52 +0200

    fix forwarder

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/client/ForwarderTestClient.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/client/ForwarderTestClient.java
@@ -1,18 +1,16 @@
 package it.pagopa.selfcare.pagopa.backoffice.client;
 
-import feign.Headers;
-import feign.Param;
 import it.pagopa.selfcare.pagopa.backoffice.config.feign.ForwarderFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import javax.ws.rs.HeaderParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "forwarder-test", url = "${rest-client.forwarder.base-url}", configuration = ForwarderFeignConfig.class)
 public interface ForwarderTestClient {
 
-    @PostMapping(value = "/forward", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_XML_VALUE)
+    @PostMapping(value = "/forward", produces = MediaType.APPLICATION_JSON_VALUE)
     String testForwardConnection(
             @RequestBody String data,
             @RequestHeader("X-Host-Url") String host,
