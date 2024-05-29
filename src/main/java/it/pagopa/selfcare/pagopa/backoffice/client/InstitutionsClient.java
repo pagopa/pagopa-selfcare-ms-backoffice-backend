f014afe0c8ae0cd92864f461c9515ea6ca4403fa
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2024-05-29 15:22:00 +0200

    [VAS-976] feat: add notice institutions api (#401)
    Co-authored-by: Jacopo Carlini <jacopo.carlini@gmail.com>
    Co-authored-by: svariant <samuele.varianti@nttdata.com>

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/client/InstitutionsClient.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/client/InstitutionsClient.java
@@ -13,7 +13,7 @@ public interface InstitutionsClient {
 
     @PostMapping(value = "/institutions/data", consumes = {
             MediaType.MULTIPART_FORM_DATA_VALUE})
-    void updateInstitutions(
+    public void updateInstitutions(
             @RequestPart("institutions-data") String institutionsDataContent,
             @RequestPart(value = "file") MultipartFile logo);
 
