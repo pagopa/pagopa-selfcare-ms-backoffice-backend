39e0edffd40e8b5978aed9d6cdcfa6800e43acfc
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2024-05-29 15:22:00 +0200

    [VAS-976] feat: add notice institutions api (#401)
    Co-authored-by: Jacopo Carlini <jacopo.carlini@gmail.com>
    Co-authored-by: svariant <samuele.varianti@nttdata.com>

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/client/InstitutionsClient.java
@@ -0,0 +1,20 @@
+package it.pagopa.selfcare.pagopa.backoffice.client;
+
+import it.pagopa.selfcare.pagopa.backoffice.config.feign.InstitutionsFeignClientConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@FeignClient(name = "institutions", url = "${rest-client.institutions.base-url}",
+        configuration = InstitutionsFeignClientConfig.class)
+public interface InstitutionsClient {
+
+    @PostMapping(value = "/institutions/data", consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE})
+    public void updateInstitutions(
+            @RequestPart("institutions-data") String institutionsDataContent,
+            @RequestPart(value = "file") MultipartFile logo);
+
+}
