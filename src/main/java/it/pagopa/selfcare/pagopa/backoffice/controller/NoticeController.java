41f6e959376b6a637d083f4e7ea407a5927a6992
gioelemella <128155546+gioelemella@users.noreply.github.com>
2024-05-30 14:51:00 +0200

    [VAS-1058] feat: Update CI - Station association API (#402)
    Co-authored-by: Alessio Cialini <alessio.cialini@emeal.nttdata.com>
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/NoticeController.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/NoticeController.java
@@ -1,6 +1,5 @@
 package it.pagopa.selfcare.pagopa.backoffice.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,11 +14,15 @@ import it.pagopa.selfcare.pagopa.backoffice.service.InstitutionsService;
 import it.pagopa.selfcare.pagopa.backoffice.util.OpenApiTableMetadata;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import javax.validation.Validator;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -34,14 +37,8 @@ public class NoticeController {
 
     private final InstitutionsService institutionsService;
 
-    private final ObjectMapper objectMapper;
-
-    private final Validator validator;
-
-    public NoticeController(InstitutionsService institutionsService, ObjectMapper objectMapper, Validator validator) {
+    public NoticeController(InstitutionsService institutionsService) {
         this.institutionsService = institutionsService;
-        this.objectMapper = objectMapper;
-        this.validator = validator;
     }
 
     /**
