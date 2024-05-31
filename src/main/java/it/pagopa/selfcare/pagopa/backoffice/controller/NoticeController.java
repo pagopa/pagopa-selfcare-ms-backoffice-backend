3478c2859e0725ba0ec16561f02958ee3bb25d29
Jacopo <jacopo.carlini@gmail.com>
2024-05-31 14:48:49 +0200

    fix infra new apim

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/NoticeController.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/NoticeController.java
@@ -14,12 +14,7 @@ import it.pagopa.selfcare.pagopa.backoffice.service.InstitutionsService;
 import it.pagopa.selfcare.pagopa.backoffice.util.OpenApiTableMetadata;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
