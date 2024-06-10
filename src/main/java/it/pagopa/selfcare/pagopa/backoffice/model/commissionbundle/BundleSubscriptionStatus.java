63752cf58b31d98f23db6f3d39f4e8f534cd0001
gioelemella <128155546+gioelemella@users.noreply.github.com>
2024-06-10 16:26:43 +0200

    [VAS-959] feat: Add CI private bundle table API (#425)

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/commissionbundle/BundleSubscriptionStatus.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/commissionbundle/BundleSubscriptionStatus.java
@@ -1,7 +1,7 @@
 package it.pagopa.selfcare.pagopa.backoffice.model.commissionbundle;
 
 /**
- * Enum that hold public bundle's subscription status
+ * Enum that hold CI bundle's subscription status
  */
 public enum BundleSubscriptionStatus {
     WAITING, ACCEPTED
