37027ab126e5fdc9c579ecba5ab7031bb3dbfab6
giomella <giomella@emeal.nttdata.com>
2024-06-20 18:04:35 +0200

    [VAS-1002] removed unused import

--- src/test/java/it/pagopa/selfcare/pagopa/backoffice/scheduler/CommissionBundleMailNotificationSchedulerTest.java
+++ src/test/java/it/pagopa/selfcare/pagopa/backoffice/scheduler/CommissionBundleMailNotificationSchedulerTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
