4c34c77fcce8c45ded634a92f475b5ae0276fa0b
Samuele Varianti <128470180+svariant@users.noreply.github.com>
2024-06-27 11:31:40 +0200

    [VAS-1133] feat: Add new api key for printPaymentsNotice product (#446)

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/Subscription.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/Subscription.java
@@ -18,7 +18,8 @@ public enum Subscription {
     FDR_ORG("/products/fdr-org", "FdR - Flussi di Rendicontazione (EC)", "fdrorg-"),
     FDR_PSP("/products/fdr-psp", "FdR - Flussi di Rendicontazione (PSP)", "fdrpsp-"),
     BO_EXT_EC("/apis/%s-backoffice-external-ec-api-v1", "Backoffice External (EC)", "selfcareboexternalec-"),
-    BO_EXT_PSP("/apis/%s-backoffice-external-psp-api-v1", "Backoffice External (PSP)", "selfcareboexternalpsp-");
+    BO_EXT_PSP("/apis/%s-backoffice-external-psp-api-v1", "Backoffice External (PSP)", "selfcareboexternalpsp-"),
+    PRINT_NOTICE("/products/pagopa_notices_service_external", "Stampa Avvisi", "printnotice-");;
 
     private final String scope;
     private final String displayName;
--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/scheduler/TaxonomiesExtractionScheduler.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/scheduler/TaxonomiesExtractionScheduler.java
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
