35f19352041198aeaa83888ce2e0170424791f89
svariant <samuele.varianti@nttdata.com>
2024-07-23 16:17:07 +0200

    [PPANTT-47] feat: Define unit test station maintenance

---
+++ src/test/java/it/pagopa/selfcare/pagopa/backoffice/controller/StationMaintenanceControllerTest.java
@@ -0,0 +1,68 @@
+package it.pagopa.selfcare.pagopa.backoffice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.selfcare.pagopa.backoffice.model.stationmaintenance.CreateStationMaintenance;
+import it.pagopa.selfcare.pagopa.backoffice.model.stationmaintenance.StationMaintenanceResource;
+import it.pagopa.selfcare.pagopa.backoffice.service.StationMaintenanceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.inject.Inject;
+import java.time.OffsetDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(username = "user1", password = "pwd", roles = "USER")
+class StationMaintenanceControllerTest {
+
+    private final static String STATION_CODE = "stationCode";
+    private final static String BROKER_CODE = "brokerCode";
+    private final static long MAINTENANCE_ID = 100;
+    @MockBean
+    private StationMaintenanceService stationMaintenanceService;
+    @Autowired
+    private MockMvc mvc;
+    @Inject
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.reset(stationMaintenanceService);
+    }
+
+    @Test
+    void createStationMaintenance() throws Exception {
+        StationMaintenanceResource response = new StationMaintenanceResource();
+        response.setStationCode(STATION_CODE);
+        response.setStandIn(true);
+        response.setEndDateTime(OffsetDateTime.now());
+        response.setStartDateTime(OffsetDateTime.now());
+        response.setMaintenanceId(MAINTENANCE_ID);
+        response.setBrokerCode(BROKER_CODE);
+        when(stationMaintenanceService.createStationMaintenance(anyString(), any())).thenReturn(response);
+
+        CreateStationMaintenance request = new CreateStationMaintenance();
+        request.setStationCode(STATION_CODE);
+        request.setStandIn(true);
+        request.setEndDateTime(OffsetDateTime.now());
+        request.setStartDateTime(OffsetDateTime.now());
+        mvc.perform(post("/brokers/{brokercode}/station-maintenances", BROKER_CODE)
+                        .content(objectMapper.writeValueAsBytes(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+}
---
+++ src/test/java/it/pagopa/selfcare/pagopa/backoffice/service/StationMaintenanceServiceTest.java
@@ -0,0 +1,61 @@
+package it.pagopa.selfcare.pagopa.backoffice.service;
+
+import it.pagopa.selfcare.pagopa.backoffice.client.ApiConfigClient;
+import it.pagopa.selfcare.pagopa.backoffice.entity.WrapperEntities;
+import it.pagopa.selfcare.pagopa.backoffice.entity.WrapperEntity;
+import it.pagopa.selfcare.pagopa.backoffice.model.connector.station.StationDetails;
+import it.pagopa.selfcare.pagopa.backoffice.model.connector.wrapper.WrapperStatus;
+import it.pagopa.selfcare.pagopa.backoffice.model.stationmaintenance.CreateStationMaintenance;
+import it.pagopa.selfcare.pagopa.backoffice.model.stationmaintenance.StationMaintenanceResource;
+import it.pagopa.selfcare.pagopa.backoffice.model.stations.StationDetailResource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.OffsetDateTime;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {StationMaintenanceService.class})
+class StationMaintenanceServiceTest {
+
+    private final static String STATION_CODE = "stationCode";
+    private final static String BROKER_CODE = "brokerCode";
+    private final static long MAINTENANCE_ID = 100;
+    @Autowired
+    private StationMaintenanceService stationMaintenanceService;
+    @MockBean
+    private ApiConfigClient apiConfigClient;
+
+    @Test
+    void createStationMaintenanceSuccess() {
+        StationMaintenanceResource response = new StationMaintenanceResource();
+        response.setStationCode(STATION_CODE);
+        response.setStandIn(true);
+        response.setEndDateTime(OffsetDateTime.now());
+        response.setStartDateTime(OffsetDateTime.now());
+        response.setMaintenanceId(MAINTENANCE_ID);
+        response.setBrokerCode(BROKER_CODE);
+
+        when(apiConfigClient.createStationMaintenance(anyString(), any(CreateStationMaintenance.class)))
+                .thenReturn(response);
+
+        CreateStationMaintenance request = new CreateStationMaintenance();
+        request.setStationCode(STATION_CODE);
+        request.setStandIn(true);
+        request.setEndDateTime(OffsetDateTime.now());
+        request.setStartDateTime(OffsetDateTime.now());
+        StationMaintenanceResource result = assertDoesNotThrow(() -> stationMaintenanceService.createStationMaintenance(BROKER_CODE, request));
+
+        assertNotNull(result);
+
+        verify(apiConfigClient).createStationMaintenance(anyString(), any(CreateStationMaintenance.class));
+    }
+}
