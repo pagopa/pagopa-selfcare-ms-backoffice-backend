410ce671392bec2169e703efea6fd639b3686ecd
svariant <samuele.varianti@nttdata.com>
2024-10-18 09:28:36 +0200

    chore: Empty row commit to re-trigger PR

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/service/ApiManagementService.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/service/ApiManagementService.java
@@ -453,6 +453,7 @@ public class ApiManagementService {
                 .build();
     }
 
+
     private DelegationInfo getDelegationInfo(
             String institutionId,
             Boolean hasAuthDelegations,
