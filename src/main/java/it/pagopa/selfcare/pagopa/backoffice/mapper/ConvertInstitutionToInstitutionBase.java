6b8f8824198f04d67a06e661b44c9b57eac2e0e9
svariant <samuele.varianti@nttdata.com>
2024-10-22 17:39:32 +0200

    fix: Conversion Institution to InstitutionBase for pagopaOperator

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/mapper/ConvertInstitutionToInstitutionBase.java
@@ -0,0 +1,22 @@
+package it.pagopa.selfcare.pagopa.backoffice.mapper;
+
+import it.pagopa.selfcare.pagopa.backoffice.model.institutions.InstitutionBase;
+import it.pagopa.selfcare.pagopa.backoffice.model.institutions.client.Institution;
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+import java.util.Collections;
+
+public class ConvertInstitutionToInstitutionBase implements Converter<Institution, InstitutionBase> {
+    @Override
+    public InstitutionBase convert(MappingContext<Institution, InstitutionBase> mappingContext) {
+        Institution src = mappingContext.getSource();
+        return InstitutionBase.builder()
+                .id(src.getId())
+                .userProductRoles(Collections.emptyList()
+                )
+                .description(src.getDescription())
+                .build();
+    }
+}
+
