612727572431ed06ad7e6c0b85ad40d2f434e73e
svariant <samuele.varianti@nttdata.com>
2025-02-25 17:48:38 +0100

    unused import

--- src/test/java/it/pagopa/selfcare/pagopa/backoffice/controller/AwsQuicksightControllerTest.java
+++ src/test/java/it/pagopa/selfcare/pagopa/backoffice/controller/AwsQuicksightControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
