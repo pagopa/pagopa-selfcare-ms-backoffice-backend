20a23e64b06dc5b9dfff30ce40bc44a926a7f06c
Jacopo Carlini <jacopo.carlini@gmail.com>
2025-02-25 09:34:38 +0100

    [PIDM-206] fix: paged get users (#538)

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/component/ApiManagementComponent.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/component/ApiManagementComponent.java
@@ -5,51 +5,55 @@ import it.pagopa.selfcare.pagopa.backoffice.model.institutions.InstitutionBase;
 import it.pagopa.selfcare.pagopa.backoffice.model.institutions.InstitutionDetail;
 import it.pagopa.selfcare.pagopa.backoffice.model.institutions.client.Institution;
 import it.pagopa.selfcare.pagopa.backoffice.model.users.client.UserInstitution;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.List;
-
 @Slf4j
 @Component
 public class ApiManagementComponent {
 
-    private final ExternalApiClient externalApiClient;
+  private final ExternalApiClient externalApiClient;
 
-    private final ModelMapper modelMapper;
+  private final ModelMapper modelMapper;
 
-    public ApiManagementComponent(ExternalApiClient externalApiClient, ModelMapper modelMapper) {
-        this.externalApiClient = externalApiClient;
-        this.modelMapper = modelMapper;
-    }
+  public ApiManagementComponent(ExternalApiClient externalApiClient, ModelMapper modelMapper) {
+    this.externalApiClient = externalApiClient;
+    this.modelMapper = modelMapper;
+  }
 
+  @Cacheable(cacheNames = "getInstitutionsForOperator")
+  public List<InstitutionBase> getInstitutionsForOperator(String taxCode) {
+    return externalApiClient.getInstitutionsFiltered(taxCode).getInstitutions().stream()
+        .map(elem -> modelMapper.map(elem, InstitutionBase.class))
+        .toList();
+  }
 
-    @Cacheable(cacheNames = "getInstitutionsForOperator")
-    public List<InstitutionBase> getInstitutionsForOperator(String taxCode) {
-        return externalApiClient.getInstitutionsFiltered(taxCode).getInstitutions().stream()
-                .map(elem -> modelMapper.map(elem, InstitutionBase.class))
-                .toList();
-    }
+  @Cacheable(cacheNames = "getInstitutions")
+  public List<InstitutionBase> getInstitutions(String userIdForAuth) {
+    List<InstitutionBase> institutionsBaseList;
 
-    @Cacheable(cacheNames = "getInstitutions")
-    public List<InstitutionBase> getInstitutions(String userIdForAuth) {
-        List<InstitutionBase> institutionsBaseList;
-        Collection<UserInstitution> institutions = externalApiClient.getUserInstitution(
-                userIdForAuth, null, null, null, null, null, null);
-        institutionsBaseList = institutions.stream()
-                .map(elem -> modelMapper.map(elem, InstitutionBase.class))
-                .toList();
-        return institutionsBaseList;
-    }
+    Collection<UserInstitution> institutions = new ArrayList<>();
+    Collection<UserInstitution> pageInstitutions;
+    int page = 0;
+    do {
+      pageInstitutions =
+          externalApiClient.getUserInstitution(userIdForAuth, null, null, null, null, page, 100);
+      institutions.addAll(pageInstitutions);
+      page += 1;
+    } while (!pageInstitutions.isEmpty());
+    institutionsBaseList =
+        institutions.stream().map(elem -> modelMapper.map(elem, InstitutionBase.class)).toList();
+    return institutionsBaseList;
+  }
 
-    @Cacheable(cacheNames = "getInstitutionDetail")
-    public InstitutionDetail getInstitutionDetail(String institutionId) {
-        Institution institution = externalApiClient.getInstitution(institutionId);
-        return modelMapper.map(institution, InstitutionDetail.class);
-    }
-
+  @Cacheable(cacheNames = "getInstitutionDetail")
+  public InstitutionDetail getInstitutionDetail(String institutionId) {
+    Institution institution = externalApiClient.getInstitution(institutionId);
+    return modelMapper.map(institution, InstitutionDetail.class);
+  }
 }
-
