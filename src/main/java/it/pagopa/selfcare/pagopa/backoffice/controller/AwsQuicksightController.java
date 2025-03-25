ccd12e6646d7016135b4b46e06ade14affa40c93
giomella <giomella@emeal.nttdata.com>
2025-03-25 12:30:52 +0100

    [PIDM-370] updated openapi

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/AwsQuicksightController.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/controller/AwsQuicksightController.java
@@ -35,14 +35,15 @@ public class AwsQuicksightController {
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = QuicksightEmbedUrlResponse.class))),
-            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
             @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
             @ApiResponse(responseCode = "500", description = "Service unavailable", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))
     })
     @Operation(summary = "Get aws quicksight dashboard's embed url", security = {@SecurityRequirement(name = "JWT")})
     @OpenApiTableMetadata(readWriteIntense = OpenApiTableMetadata.ReadWrite.READ)
     public QuicksightEmbedUrlResponse getEmbedUrlForAnonymousUser(
-            @Parameter(description = "Institution's id for dashboard retrieval(Only for operator PagoPA)") @RequestParam(required = false) String institutionId
+            @Parameter(description = "Institution's id for dashboard retrieval (Only for operator PagoPA)") @RequestParam(required = false) String institutionId
     ) {
         return this.awsQuicksightService.generateEmbedUrlForAnonymousUser(institutionId);
     }
