d910815e4fb9246f3dab23d1101c234fca4e33ab
Pietro Tota <pietro.tota@pagopa.it>
2025-04-24 11:52:33 +0200

    feat: update helm chart version

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -60,3 +60,9 @@ data "azurerm_key_vault_secret" "key_vault_integration_test_subkey" {
   name         = "integration-test-subkey"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+
+data "azurerm_user_assigned_identity" "workload_identity_clientid" {
+  name                = "qi-workload-identity"
+  resource_group_name = "pagopa-${var.env_short}-weu-${var.env}-aks-rg"
+}
