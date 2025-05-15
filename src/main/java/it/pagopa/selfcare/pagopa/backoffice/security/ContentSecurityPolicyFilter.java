cc011e11cdf96628005d3b1d7d89e690ae72f6a0
Francesco Di Tonno <francesco.ditonno@bvtech.com>
2025-05-15 16:29:09 +0200

    fix: [CHK-4137] Content Security Policy

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/security/ContentSecurityPolicyFilter.java
@@ -0,0 +1,29 @@
+package it.pagopa.selfcare.pagopa.backoffice.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ContentSecurityPolicyFilter implements Filter {
+
+    private String imgSource;
+
+    public ContentSecurityPolicyFilter(
+            @Value("${rest-client.external-api.printit-blob-url}") String imgSource
+    ) {
+        this.imgSource = imgSource;
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResp = (HttpServletResponse) servletResponse;
+        httpResp.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' " + imgSource + ";");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
