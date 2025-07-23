b2682311d21c3a6fa3462e562875fe2e500c1041
Pietro Tota <pietro.tota@pagopa.it>
2025-07-23 10:32:54 +0200

    chore: run docker script shellcheck suggestions

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -32,25 +32,25 @@ if [ "$ENV" = "local" ]; then
   image="service-local:latest"
   ENV="dev"
   cp .env.local .env
-  echo "\nDOCKER_COMPOSE_MONGO_REPLICAS=1" >> .env
+  printf "\nDOCKER_COMPOSE_MONGO_REPLICAS=1" >> .env
 else
-  repository=$(yq -r '."microservice-chart".image.repository' ../helm/values-$ENV.yaml)
+  repository=$(yq -r '."microservice-chart".image.repository' ../helm/values-"$ENV".yaml)
     image="${repository}:latest"
   #generating env file from helm values
-  echo "DOCKER_COMPOSE_MONGO_REPLICAS=0" >> .env
-  config=$(yq  -r '."microservice-chart".envConfig' ../helm/values-$ENV.yaml)
+  printf "DOCKER_COMPOSE_MONGO_REPLICAS=0\n" >> .env
+  config=$(yq  -r '."microservice-chart".envConfig' ../helm/values-"$ENV".yaml)
   IFS=$'\n'
   for line in $(echo "$config" | yq -r '. | to_entries[] | select(.key) | "\(.key)=\(.value)"'); do
       echo "$line" >> .env
   done
 
 
-  keyvault=$(yq  -r '."microservice-chart".keyvault.name' ../helm/values-$ENV.yaml)
-  secret=$(yq  -r '."microservice-chart".envSecret' ../helm/values-$ENV.yaml)
+  keyvault=$(yq  -r '."microservice-chart".keyvault.name' ../helm/values-"$ENV".yaml)
+  secret=$(yq  -r '."microservice-chart".envSecret' ../helm/values-"$ENV".yaml)
   for line in $(echo "$secret" | yq -r '. | to_entries[] | select(.key) | "\(.key)=\(.value)"'); do
     IFS='=' read -r -a array <<< "$line"
     echo "Reading secret $keyvault.${array[1]}"
-    response=$(az keyvault secret show --vault-name $keyvault --name "${array[1]}")
+    response=$(az keyvault secret show --vault-name "$keyvault" --name "${array[1]}")
     response=$(echo "$response" | tr -d '\n')
     value=$(echo "$response" | yq -r '.value')
     value=$(echo "$value" | sed 's/\$/\$\$/g')
@@ -68,7 +68,7 @@ docker compose -p "${stack_name}" up -d --remove-orphans --force-recreate --buil
 printf 'Waiting for the service'
 attempt_counter=0
 max_attempts=50
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/actuator/info); do
+until curl --output /dev/null --silent --head --fail http://localhost:8080/actuator/info; do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached"
       exit 1
