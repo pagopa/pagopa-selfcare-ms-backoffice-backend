db03775228450d6b92673496c3aaedd7eeee0e00
Forciniti Luigi <forciniti@spi.srl>
2025-10-24 16:45:56 +0200

    fix: Add PaymentType with paymentTypeCode=ANY to getPaymentTypes

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/service/ConfigurationService.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/service/ConfigurationService.java
@@ -1,11 +1,14 @@
 package it.pagopa.selfcare.pagopa.backoffice.service;
 
 import it.pagopa.selfcare.pagopa.backoffice.client.ApiConfigClient;
+import it.pagopa.selfcare.pagopa.backoffice.model.configuration.PaymentType;
 import it.pagopa.selfcare.pagopa.backoffice.model.configuration.PaymentTypes;
 import it.pagopa.selfcare.pagopa.backoffice.model.configuration.WfespPluginConfs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Service
@@ -15,7 +18,15 @@ public class ConfigurationService {
     private ApiConfigClient apiConfigClient;
 
     public PaymentTypes getPaymentTypes() {
-        return apiConfigClient.getPaymentTypes();
+        @Valid PaymentTypes paymentTypes = apiConfigClient.getPaymentTypes();
+        if(paymentTypes != null && paymentTypes.getPaymentTypeList() != null && !paymentTypes.getPaymentTypeList().isEmpty())
+        {
+            PaymentType paymentTypeAny = new PaymentType();
+            paymentTypeAny.setDescription("");
+            paymentTypeAny.setPaymentTypeCode("ANY");
+            paymentTypes.getPaymentTypeList().add(paymentTypeAny);
+        }
+        return paymentTypes;
     }
 
     public WfespPluginConfs getWfespPlugins() {
