a887fa0cd0c963d4bb1753225ffcfe6df5eca978
ivan.ferrando <ivan.ferrando@pagopa.it>
2026-02-02 12:55:17 +0100

    feat(service-consent): add API definition

---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/ServiceConsentsResponse.java
@@ -0,0 +1,21 @@
+package it.pagopa.selfcare.pagopa.backoffice.model.institutions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceConsentsResponse {
+
+    @Schema(description = "List of services with expressed consent")
+    @JsonProperty("services")
+    @NotNull
+    private List<ServiceConsentInfo> services;
+}
---
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/model/institutions/ServiceId.java
@@ -0,0 +1,11 @@
+package it.pagopa.selfcare.pagopa.backoffice.model.institutions;
+
+/**
+ * Enumeration of institution services
+ */
+public enum ServiceId {
+    /**
+     * Request to pay
+     */
+    RTP
+}
