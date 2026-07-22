322a6c1f0b7234bf93ba5d106cffe8d03a8d1d39
svariant <samuele.varianti@nttdata.com>
2026-07-22 17:09:08 +0200

    fix: [PIDM-2200] Unused import

--- src/main/java/it/pagopa/selfcare/pagopa/backoffice/entity/CreditorInstitutionIbansEntity.java
+++ src/main/java/it/pagopa/selfcare/pagopa/backoffice/entity/CreditorInstitutionIbansEntity.java
@@ -4,7 +4,6 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
